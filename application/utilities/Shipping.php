40cc94bf80012c6d9201b8d49b0e2fa56d543651
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-01-18 18:16:32 +0530

    task-66150-Add system log functionality if dimension detail missing for any product

--- application/utilities/Shipping.php
+++ application/utilities/Shipping.php
@@ -252,7 +252,10 @@ class Shipping
         $dimensionUnits = ShippingPackage::getUnitTypes($this->langId);
         foreach ($this->selProdShipRates as $rateId => $rates) {
             $product = $productInfo[$rates['selprod_id']];
-            if (empty($product['shippack_length']) || empty($product['shippack_width']) || empty($product['shippack_height']) || empty($product['shippack_units'])) {
+            if (!empty($product['shippack_length']) || empty($product['shippack_width']) || empty($product['shippack_height']) || empty($product['shippack_units'])) {
+                $msg = Labels::getLabel('MSG_MISSING_LENGTH_/_WIDTH_/_HEIGHT_OR_UNIT_PARAMS_FOR_"{PRODUCT}"._PLEASE_BIND_CORRECT_PACKAGE.', $this->langId);
+                $msg = CommonHelper::replaceStringData($msg, ['{PRODUCT}' => $product['selprod_title']]);
+                SystemLog::set($msg);
                 continue;
             }
 
@@ -273,6 +276,9 @@ class Shipping
                 /*  $user = self::BY_ADMIN == $shippedBy ? Labels::getLabel('MSG_ADMIN', $this->langId) : Labels::getLabel('MSG_SHOP', $this->langId);
                  $error = Labels::getLabel('MSG_UNABLE_TO_LOCATE_{USER}_POSTAL_CODE', $this->langId);
                  $this->error = CommonHelper::replaceStringData($error, ['{USER}' => $user]); */
+                $msg = Labels::getLabel('MSG_MISSING_FROM_ZIP_FOR_"{PRODUCT}"._PLEASE_FROM_ADDRESS.', $this->langId);
+                $msg = CommonHelper::replaceStringData($msg, ['{PRODUCT}' => $product['selprod_title']]);
+                SystemLog::set($msg);
                 continue;
             }
 
