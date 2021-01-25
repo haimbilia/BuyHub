b38c0a2ef8e81439f0519e0d7f31c82a0f601380
ablysunil <sunil.kumar@fatbit.in>
2021-01-25 13:56:33 +0530

    task-80928-added settings form in admin for mollie payment gateway

---
+++ database/db-update.sql
@@ -0,0 +1,3 @@
+-- --- Mollie Payment Gateway--- --
+INSERT IGNORE INTO `tbl_plugins` (`plugin_identifier`, `plugin_type`, `plugin_code`, `plugin_active`, `plugin_display_order`) VALUES ('Mollie', '13', 'Mollie', '0', '25');
+-- --- Mollie Payment Gateway--- --
