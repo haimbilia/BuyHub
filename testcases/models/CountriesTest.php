e1d76be971c2da14efbb0d8d7ff7e8d0a5423e17
Vivek Kumar Maurya <vivek.kumar@fatbit.in>
2021-01-29 17:04:26 +0530

    task-74409 #test cases of countries and states model

--- testcases/models/CountriesTest.php
+++ testcases/models/CountriesTest.php
@@ -33,6 +33,7 @@ class CountriesTest extends YkModelTest
     public static function truncateDbData()
     {
         FatApp::getDb()->query("TRUNCATE TABLE ".Countries::DB_TBL);
+        FatApp::getDb()->query("TRUNCATE TABLE ".Countries::DB_TBL_LANG);
     }
     
     private function insertCountriesData()
@@ -42,10 +43,10 @@ class CountriesTest extends YkModelTest
                 'country_id'=>1, 'country_code'=>'AL', 'country_code_alpha3'=>'ALB', 'country_active'=>1, 'country_zone_id'=>2, 'country_currency_id'=>0, 'country_language_id'=>0
             ],
             [
-                'country_id'=>1, 'country_code'=>'AF', 'country_code_alpha3'=>'AFG', 'country_active'=>1, 'country_zone_id'=>4, 'country_currency_id'=>0, 'country_language_id'=>0
+                'country_id'=>2, 'country_code'=>'AF', 'country_code_alpha3'=>'AFG', 'country_active'=>1, 'country_zone_id'=>4, 'country_currency_id'=>0, 'country_language_id'=>0
             ],
             [
-                'country_id'=>1, 'country_code'=>'DZ', 'country_code_alpha3'=>'DZA', 'country_active'=>0, 'country_zone_id'=>1, 'country_currency_id'=>0, 'country_language_id'=>0
+                'country_id'=>3, 'country_code'=>'DZ', 'country_code_alpha3'=>'DZA', 'country_active'=>0, 'country_zone_id'=>1, 'country_currency_id'=>0, 'country_language_id'=>0
             ]
         ];
         $this->InsertDbData(Countries::DB_TBL, $arr);
@@ -77,7 +78,7 @@ class CountriesTest extends YkModelTest
      * @param  string $idCol
      * @return void
      */
-    public function getCountriesArr($expected, $langId, $isActive, $idCol)
+    public function getCountriesArr($expected, $langId, $isActive, $idCol = 'country_id')
     {
         $this->expectedReturnType(YkAppTest::TYPE_ARRAY);
         $result = $this->execute($this->class, [], 'getCountriesArr', [$langId, $isActive, $idCol]);
@@ -92,11 +93,67 @@ class CountriesTest extends YkModelTest
     public function feedGetCountriesArr()
     {  
         return [
-            [0, 'test', 0, ''],   //Invalid langId, valid recordId, expected 0 country
-            [0, 1, 'test', ''],   //Valid langId, Invalid isActive, expected 0 country
-            [2, 1, true, ''],   //Valid type, valid recordId, Invalid langId, expected 2 countries
-            [1, 1, false, ''],   //Valid type, valid recordId, valid langId, expected 1 country
+            [2, 1, 'test'],   //Valid langId, Invalid isActive
+            [2, 1, true],   //Valid langId, Valid isActive
+            [3, 1, false],  //Valid langId, Valid isActive
         ];
     } 
+
+    /**
+     * @test
+     *
+     * @dataProvider feedGetCountryByCode
+     * @param int countryCode
+     * @param string attr
+     * @return void
+     */
+    public function getCountryByCode( $expected, $countryCode, $attr )
+    {
+        $countryObj = new Countries();    
+        $result = $countryObj->getCountryByCode($countryCode, $attr);
+        $this->$expected($result);
+    }
+     /**
+     * feedgetCountryByCode
+     *
+     * @return array
+     */ 
+    public function feedGetCountryByCode()
+    {  
+        return array(
+            array('assertFalse', '', null), //Invalid countryCode
+            array('assertIsInt', 'AL', 'country_id'), //Valid countryCode
+            array('assertIsArray', 'AL', null), //Valid countryCode
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider feedGetCountryById
+     * @param int countryId
+     * @param int langId
+     * @param string attr
+     * @return void
+     */
+    public function getCountryById( $expected, $countryId, $langId, $attr )
+    {
+        $countryObj = new Countries();    
+        $result = $countryObj->getCountryById($countryId, $langId, $attr);
+        $this->$expected($result);
+    }
+     /**
+     * feedGetCountryById
+     *
+     * @return array
+     */ 
+    public function feedGetCountryById()
+    {  
+        return array(
+            array('assertFalse', '', '', null), //Invalid countryId
+            array('assertIsInt', 1, 1, 'country_id'), //Valid countryId
+            array('assertIsArray', 1, 1, null), //Valid countryId
+        );
+    }
 
 }
