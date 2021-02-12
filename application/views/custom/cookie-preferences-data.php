b875db8f45e6fd1a05c4d27575a8b591489f884b
pawan kumar <pawan.kumar.dz@ablysoft.com>
2021-02-12 15:37:45 +0530

    task-82123-worked on UI/UX tweaks

--- application/views/custom/cookie-preferences-data.php
+++ application/views/custom/cookie-preferences-data.php
@@ -1,32 +1,42 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-<div class="cookie-alert">
-    <h5><?php echo Labels::getLabel('LBL_Your_Cookie_Preferences', $siteLangId); ?></h5>
+<div class="cookie-alert cms">
+    <h4> <strong> <?php echo Labels::getLabel('LBL_Your_Cookie_Preferences', $siteLangId); ?> </strong><br><br></h4>
 
-    <div><?php echo Labels::getLabel('LBL_What_is_a_cookie?', $siteLangId); ?></div>
-    <div><?php echo Labels::getLabel('LBL_What_is_a_cookie_Information', $siteLangId); ?></div>
+    <div>
+        <h5> <?php echo Labels::getLabel('LBL_What_is_a_cookie?', $siteLangId); ?> </h5>
+    </div>
+    <div>
+        <p> <?php echo Labels::getLabel('LBL_What_is_a_cookie_Information', $siteLangId); ?></p>
+    </div>
 
     <div>
         <label class="checkbox">
             <input type="checkbox" name="functional_cookies" disabled="disabled" checked="true">
             <strong><?php echo Labels::getLabel('LBL_Functional_cookies(non-optional)', $siteLangId); ?></strong>
-        </label>
-        <div><?php echo Labels::getLabel('LBL_Functional_Cookies_Information', $siteLangId); ?></div>
+        </label> <br><br>
+        <div>
+            <p> <?php echo Labels::getLabel('LBL_Functional_Cookies_Information', $siteLangId); ?></p>
+        </div>
     </div>
 
     <div>
         <label class="checkbox">
             <input type="checkbox" name="statistical_cookies" checked="true">
             <strong><?php echo Labels::getLabel('LBL_Statistical_analysis_cookies', $siteLangId); ?></strong>
-        </label>
-        <div><?php echo Labels::getLabel('LBL_Statistical_Analysis_Cookies_Information', $siteLangId); ?></div>
+        </label><br><br>
+        <div>
+            <p> <?php echo Labels::getLabel('LBL_Statistical_Analysis_Cookies_Information', $siteLangId); ?></p>
+        </div>
     </div>
 
     <div>
         <label class="checkbox">
-            <input type="checkbox" name="personalise_cookies"  checked="true">
+            <input type="checkbox" name="personalise_cookies" checked="true">
             <strong><?php echo Labels::getLabel('LBL_Cookies_to_personalise_the_experience', $siteLangId); ?></strong>
-        </label>
-        <div><?php echo Labels::getLabel('LBL_Personalise_Cookies_Information', $siteLangId); ?></div>
+        </label><br><br>
+        <div>
+            <p> <?php echo Labels::getLabel('LBL_Personalise_Cookies_Information', $siteLangId); ?></p>
+        </div>
     </div>
 
     <div>
