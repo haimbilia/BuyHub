6ce80f02bc8d649e8a81780238f2437df5e33909
Anup Singh Rawat <anup.rawat@ablysoft.com>
2021-03-04 18:10:26 +0530

    task-82396 performance optimization

--- database/db-update.sql
+++ database/db-update.sql
@@ -5,3 +5,6 @@ UPDATE `tbl_plugins` SET `plugin_identifier`='Paygate', `plugin_code`='Paygate' 
 -- --- Dpo Payment Gateway--- --
 INSERT IGNORE INTO `tbl_plugins` (`plugin_identifier`, `plugin_type`, `plugin_code`, `plugin_active`, `plugin_display_order`) VALUES ('Dpo', '13', 'Dpo', '0', '23');
 -- --- Dpo Payment Gateway--- --
+ALTER TABLE `tbl_shops` ADD INDEX( `shop_country_id`);
+ALTER TABLE `tbl_shops` ADD INDEX( `shop_state_id`);
+ALTER TABLE `tbl_products` ADD INDEX( `product_ship_package`);
