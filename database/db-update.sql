b38a719ad946c534cc0eb725949f6d701e823ca1
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-04-19 11:23:43 +0530

    task-72808-Db update file query update

--- database/db-update.sql
+++ database/db-update.sql
@@ -1,10 +1,11 @@
+-- --- Dpo Payment Gateway--- --
+INSERT IGNORE INTO `tbl_plugins` (`plugin_identifier`, `plugin_type`, `plugin_code`, `plugin_active`, `plugin_display_order`) VALUES ('Dpo', '13', 'Dpo', '0', '23');
+-- --- Dpo Payment Gateway--- --
+
 /* Renaming DPO payment gateway to Paygate. */
 UPDATE `tbl_plugins` SET `plugin_identifier`='Paygate', `plugin_code`='Paygate' WHERE plugin_code = 'Dpo';
 /* Renaming DPO payment gateway to Paygate. */
 
--- --- Dpo Payment Gateway--- --
-INSERT IGNORE INTO `tbl_plugins` (`plugin_identifier`, `plugin_type`, `plugin_code`, `plugin_active`, `plugin_display_order`) VALUES ('Dpo', '13', 'Dpo', '0', '23');
--- --- Dpo Payment Gateway--- --
 ALTER TABLE `tbl_shops` ADD INDEX( `shop_country_id`);
 ALTER TABLE `tbl_shops` ADD INDEX( `shop_state_id`);
 ALTER TABLE `tbl_products` ADD INDEX( `product_ship_package`);
