4183aeb74b80378a4bf489b08155fcc425f53d6b
ablyanup <anup.rawat@ablysoft.com>
2021-05-20 17:39:40 +0530

    task-83850 admin earnings report

--- admin-application/views/_partial/header/left-navigation.php
+++ admin-application/views/_partial/header/left-navigation.php
@@ -360,7 +360,7 @@
                           <?php if ($objPrivilege->canViewFinancialReport(AdminAuthentication::getLoggedAdminId(), true)) { ?>
                               <li class="child"><a href="javascript:void(0);"><?php echo Labels::getLabel('LBL_Financial_Report', $adminLangId); ?></a>
                                   <ul>
-                                      <?php /* <li><a href="<?php echo UrlHelper::generateUrl('EarningsReport'); ?>"><?php echo Labels::getLabel('LBL_Earnings', $adminLangId); ?></a></li> */ ?>
+                                      <li><a href="<?php echo UrlHelper::generateUrl('EarningsReport'); ?>"><?php echo Labels::getLabel('LBL_Earnings', $adminLangId); ?></a></li>
                                       <li><a href="<?php echo UrlHelper::generateUrl('ProductProfitReport'); ?>"><?php echo Labels::getLabel('LBL_Products', $adminLangId); ?></a></li>
 
                                   </ul>
