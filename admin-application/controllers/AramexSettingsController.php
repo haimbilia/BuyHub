dee50e478c07e65f6d50c47866b17825baea91fc
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-05-20 13:45:46 +0530

    task-85369-remove extra code

--- admin-application/controllers/AramexSettingsController.php
+++ admin-application/controllers/AramexSettingsController.php
@@ -8,7 +8,7 @@ class AramexSettingsController extends ShippingServicesSettingsController
         $frm = new Form('frm' . self::KEY_NAME);
 
         $envoirment = Plugin::getEnvArr($langId);
-        $envFld = $frm->addSelectBox(Labels::getLabel('LBL_ENVOIRMENT', $langId), 'env', $envoirment, '', ['class' => 'fieldsVisibility-js'], '');
+        $envFld = $frm->addSelectBox(Labels::getLabel('LBL_ENVOIRMENT', $langId), 'env', $envoirment);
         $envFld->requirement->setRequired(true);
 
         $plugin = PluginHelper::callPlugin(self::KEY_NAME, [$langId], $error, $langId, false);
