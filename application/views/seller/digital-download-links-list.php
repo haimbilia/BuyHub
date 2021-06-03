aff3e8fdb74a304150bca6866eacc2953f971002
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-06-03 16:19:53 +0530

    bug-054212 inventory UI fixed on multiple locations

--- application/views/seller/digital-download-links-list.php
+++ application/views/seller/digital-download-links-list.php
@@ -41,10 +41,10 @@ foreach ($links as $sn => $row) {
                 $td->appendElement('plaintext', array(), $val, true);
                 break;
             case 'pdl_download_link':
-                $td->appendElement('div', array("class"=>"clipboard"), '<input class="copy-input" value="'.$row[$key].'" id="copymain_'. $row['pdl_id'] .'" readonly> <button class="copy-btn" id="copyButton_'. $row['pdl_id'] .'" onclick="fcom.copyToClipboard(\'copymain_'. $row['pdl_id'] .'\')"><i class="far fa-copy"></i></button>', true);
+                $td->appendElement('div', array("class"=>"clipboard"), '<input class="copy-input" value="'.$row[$key].'" id="copymain_'. $row['pdl_id'] .'" readonly> <button class="btn btn-light btn-sm copy-btn" id="copyButton_'. $row['pdl_id'] .'" onclick="fcom.copyToClipboard(\'copymain_'. $row['pdl_id'] .'\')"><i class="far fa-copy"></i></button>', true);
                 break;
             case 'pdl_preview_link':
-                $td->appendElement('div', array("class"=>"clipboard"), '<input class="copy-input" value="'.$row[$key].'" id="copypreview_'. $row['pdl_id'] .'" readonly> <button class="copy-btn" id="copyButton_'. $row['pdl_id'] .'" onclick="fcom.copyToClipboard(\'copypreview_'. $row['pdl_id'] .'\')"><i class="far fa-copy"></i></button>', true);
+                $td->appendElement('div', array("class"=>"clipboard"), '<input class="copy-input" value="'.$row[$key].'" id="copypreview_'. $row['pdl_id'] .'" readonly> <button class="btn btn-light btn-sm copy-btn" id="copyButton_'. $row['pdl_id'] .'" onclick="fcom.copyToClipboard(\'copypreview_'. $row['pdl_id'] .'\')"><i class="far fa-copy"></i></button>', true);
                 break;
             case 'action':
                 $td->appendElement(
