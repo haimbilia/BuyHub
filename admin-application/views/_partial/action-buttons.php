b3a610309e2280f18d6bf0336aeb4feca5b3d120
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-06-21 13:07:36 +0530

    task-76848 worked on report buttons UI

--- admin-application/views/_partial/action-buttons.php
+++ admin-application/views/_partial/action-buttons.php
@@ -10,19 +10,19 @@ if (isset($htmlContent) && $htmlContent != '') {
 
 $msg = isset($msg) ? $msg : '';
 if ((!isset($statusButtons) || true === $statusButtons)) {
-    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Publish', $adminLangId), "onclick" => "toggleBulkStatues(1, '" . $msg . "')"), '<i class="fas fa-eye"></i>', true);
+    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn btn-secondary btn-sm toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Publish', $adminLangId), "onclick" => "toggleBulkStatues(1, '" . $msg . "')"), '<i class="fas fa-eye"></i>', true);
 
-    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Unpublish', $adminLangId), "onclick" => "toggleBulkStatues(0, '" . $msg . "')"), '<i class="fas fa-eye-slash"></i>', true);
+    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn btn-secondary btn-sm toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Unpublish', $adminLangId), "onclick" => "toggleBulkStatues(0, '" . $msg . "')"), '<i class="fas fa-eye-slash"></i>', true);
 }
 
 if (!isset($deleteButton) || true === $deleteButton) {
-    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn-clean btn-sm btn-icon btn-secondary toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Delete', $adminLangId), "onclick" => "deleteSelected()"), '<i class="fas fa-trash"></i>', true);
+    $div->appendElement('a', array('href' => 'javascript:void(0)', 'class' => 'btn btn-secondary btn-sm toolbar-btn-js d-none', 'title' => Labels::getLabel('LBL_Delete', $adminLangId), "onclick" => "deleteSelected()"), '<i class="fas fa-trash"></i>', true);
 }
 
 if (isset($otherButtons) && is_array($otherButtons)) {
     foreach ($otherButtons as $attr) {
         $class = isset($attr['attr']['class']) ? $attr['attr']['class'] : '';
-        $attr['attr']['class'] = 'btn-clean btn-sm btn-icon btn-secondary ' . $class;
+        $attr['attr']['class'] = 'btn btn-secondary btn-sm' . $class;
         $div->appendElement('a', $attr['attr'], (string) $attr['label'], true);
     }
 }
