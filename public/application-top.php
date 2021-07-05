f8541d1ca962f672eade75203650a61aa7d6fc73
ablyanup <anup.rawat@ablysoft.com>
2021-07-05 16:32:53 +0530

    release test version TV-9.3.1.20210705

--- public/application-top.php
+++ public/application-top.php
@@ -64,4 +64,4 @@ if (strpos(CONF_UPLOADS_PATH, 's3://') !== false) {
     AttachedFile::registerS3ClientStream();
 }
 define('SYSTEM_INIT', true);
-define('CONF_WEB_APP_VERSION', 'TV-9.3.1.20210703');
+define('CONF_WEB_APP_VERSION', 'TV-9.3.1.20210705');
