61f6bac358a9c5af265d70abf358a23f28311c75
ablyanup <anup.rawat@ablysoft.com>
2021-07-15 12:37:33 +0530

    task-83837 dashboard files updates

--- dashboard/views/_partial/footerTrustBanners.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-
-<?php if($footerData){ ?>
-<section class="upperContent-footer">
-    <div class="container">
-	 <?php echo FatUtility::decodeHtmlEntities($footerData['epage_content'] );?>
-	</div>
-</section>
-<?php } ?>
--- dashboard/views/common-js/function.js
+++ dashboard/views/common-js/function.js
@@ -176,27 +176,7 @@ $(function () {
 
 
 /*back-top*/
-$(document).ready(function () {
-    // hide #back-top first
-    $(".back-to-top").hide();
-
-    // fade in #back-top
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 100) {
-                $('.back-to-top').fadeIn();
-            } else {
-                $('.back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('.back-to-top a').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-    });
+$(document).ready(function () {   
 
     $('.switch-button').click(function () {
         $(this).toggleClass("is--active");
