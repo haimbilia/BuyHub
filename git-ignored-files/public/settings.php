9b7c3c42e223d13af1ebae4e6cf1bd67ea3f2ae5
ablyanup <anup.rawat@ablysoft.com>
2021-07-16 10:24:35 +0530

    task-83837 updated dashboard files

--- git-ignored-files/public/settings.php
+++ git-ignored-files/public/settings.php
@@ -2,6 +2,7 @@
 // DB
 define('CONF_WEBROOT_FRONTEND', '/');
 define('CONF_WEBROOT_BACKEND', '/admin/');
+define('CONF_WEBROOT_DASHBOARD', '/dashboard/');
 //die('Domain specific settings file missing');
 define('CONF_DB_SERVER', 'localhost');
 define('CONF_DB_USER', 'root');
