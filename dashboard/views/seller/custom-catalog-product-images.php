9b292645c8598a8d68123ce2603e3f4031d35602
anant <anant.kumar.singh@fatbit.in>
2021-07-19 13:06:28 +0530

    task-87653 changed pop-up html code for tag,custom catalog product images,shipping tabsshipping packages and tag lang form

--- dashboard/views/seller/custom-catalog-product-images.php
+++ dashboard/views/seller/custom-catalog-product-images.php
@@ -14,33 +14,39 @@ $img_fld = $imagesFrm->getField('prod_image');
 $img_fld->addFieldTagAttribute('class', 'btn  btn-sm');
 $img_fld->addFieldTagAttribute('onChange', 'popupImage(this)');
 ?>
-<div class="tabs_data">
-    <div class="tabs_body">
-        <?php echo $imagesFrm->getFormHtml(); ?>
-        <div id="imageupload_div"></div>
-    </div>
-    <div class="row tabs_footer">
-        <div class="col-6">
-            <div class="field-set">
-                <div class="caption-wraper"><label class="field_label"></label></div>
-                <div class="field-wraper">
-                    <div class="field_cover">
-                        <input onclick="
-                        <?php if ($productType == Product::PRODUCT_TYPE_PHYSICAL) { ?>
-                            productShipping(<?php echo $preqId; ?>);
-                        <?php } else { ?>
-                            productOptionsAndTag(<?php echo $preqId; ?>);
-                        <?php }?>" class="btn btn-outline-brand" type="button" name="btn_back" value="<?php echo Labels::getLabel('LBL_Back', $siteLangId); ?>">
+
+<div class="modal-header">
+	<h5 class="modal-title"><?php echo Labels::getLabel('LBL_Custom_catalog_product_images',$siteLangId) ;?></h5>
+</div>
+<div class="modal-body">
+    <div class="tabs_data">
+        <div class="tabs_body">
+            <?php echo $imagesFrm->getFormHtml(); ?>
+            <div id="imageupload_div"></div>
+        </div>
+        <div class="row tabs_footer">
+            <div class="col-6">
+                <div class="field-set">
+                    <div class="caption-wraper"><label class="field_label"></label></div>
+                    <div class="field-wraper">
+                        <div class="field_cover">
+                            <input onclick="
+                            <?php if ($productType == Product::PRODUCT_TYPE_PHYSICAL) { ?>
+                                productShipping(<?php echo $preqId; ?>);
+                            <?php } else { ?>
+                                productOptionsAndTag(<?php echo $preqId; ?>);
+                            <?php }?>" class="btn btn-outline-brand" type="button" name="btn_back" value="<?php echo Labels::getLabel('LBL_Back', $siteLangId); ?>">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-6 text-right">
-            <div class="field-set">
-                <div class="caption-wraper"><label class="field_label"></label></div>
-                <div class="field-wraper">
-                    <div class="field_cover">
-                        <input onclick="goToCatalogRequest();" type="button" class="btn btn-brand" name="btn_Finish" value="<?php echo Labels::getLabel('LBL_Finish', $siteLangId); ?>">
+            <div class="col-6 text-right">
+                <div class="field-set">
+                    <div class="caption-wraper"><label class="field_label"></label></div>
+                    <div class="field-wraper">
+                        <div class="field_cover">
+                            <input onclick="goToCatalogRequest();" type="button" class="btn btn-brand" name="btn_Finish" value="<?php echo Labels::getLabel('LBL_Finish', $siteLangId); ?>">
+                        </div>
                     </div>
                 </div>
             </div>
