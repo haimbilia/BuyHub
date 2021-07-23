6e211588a65fead349c27a56ef6c71dc1039890e
ablyshashi <shashi.kumar@fatbit.in>
2021-07-23 18:20:06 +0530

    task-87972-shop review page design updated

--- application/views/shops/policy.php
+++ application/views/shops/policy.php
@@ -1,22 +1,11 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage');
-$searchFrm->setFormTagAttribute('onSubmit', 'searchProducts(this); return(false);');
-$keywordFld = $searchFrm->getField('keyword');
-$keywordFld->addFieldTagAttribute('placeholder', Labels::getLabel('LBL_Search', $siteLangId));
-$keywordFld->htmlAfterField = '<input value="" type="submit" class="input-submit">';
 $bgUrl = UrlHelper::generateFullUrl('Image', 'shopBackgroundImage', array($shop['shop_id'],$siteLangId,0,0,$template_id));
 $haveBannerImage = AttachedFile::getMultipleAttachments(AttachedFile::FILETYPE_SHOP_BANNER, $shop['shop_id'], '', $siteLangId);
-$shopPolicyArr = array(
-    'shop_payment_policy',
-    'shop_delivery_policy',
-    'shop_refund_policy',
-    'shop_additional_info',
-    'shop_seller_info',
-);
 ?>
 
 <div id="body" class="body template-<?php echo $template_id;?>"   >
     <?php
-        $variables= array('shop'=>$shop, 'siteLangId'=>$siteLangId,'frmProductSearch'=>$frmProductSearch,'template_id'=>$template_id,'collectionData'=>$collectionData,'action'=>$action,'shopId'=>$shopId,'shopTotalReviews'=>$shopTotalReviews,'shopRating'=>$shopRating);
+        $variables= array('shop'=>$shop, 'siteLangId'=>$siteLangId,'frmProductSearch'=>$frmProductSearch,'template_id'=>$template_id,'collectionData'=>$collectionData,'action'=>$action,'shopId'=>$shopId,'shopTotalReviews'=>$shopTotalReviews,'shopRating'=>$shopRating,'socialPlatforms' => $socialPlatforms,);
         $this->includeTemplate('shops/templates/'.$template_id.'.php', $variables, false);
     ?>
 
@@ -78,7 +67,6 @@ $shopPolicyArr = array(
     <?php echo $this->includeTemplate('_partial/shareThisScript.php'); ?>
     <script type="text/javascript">
     (function($){
-    searchProducts(document.frmProductSearch);
     if(langLbl.layoutDirection == 'rtl'){
         $('.shops-sliders').slick({
             dots: false,
