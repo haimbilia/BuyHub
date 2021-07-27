6eb97f82c93ebd24e14e17fdb6d3b602b0909c1c
Anup Rawat <anuprawat@Anups-Mac-mini.local>
2021-07-27 15:41:30 +0530

    task-81594 image cache optimization in home collection

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -742,7 +742,7 @@ class HomeController extends MyAppController
                     if (false === MOBILE_APP_API_CALL) {
                         $shopObj->setPageSize($collection['collection_primary_records']);
                     }
-                    $shopObj->addMultipleFields(array('ctr.ctr_display_order', 'shop_id', 'shop_user_id', 'IFNULL(shop_name, shop_identifier) as shop_name', 'IFNULL(country_name, country_code) as country_name', 'IFNULL(state_name, state_identifier) as state_name'));
+                    $shopObj->addMultipleFields(array('ctr.ctr_display_order', 'shop_id', 'shop_user_id', 'IFNULL(shop_name, shop_identifier) as shop_name', 'IFNULL(country_name, country_code) as country_name', 'IFNULL(state_name, state_identifier) as state_name', 'shop_updated_on'));
                     $shopObj->addOrder('ctr.ctr_display_order', 'ASC');
 
                     $rs = $shopObj->getResultSet();
