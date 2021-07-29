8dc80b48f8668af28f7755dee188d4ae0deaea4d
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-07-29 13:14:16 +0530

    task-87972 fixed testimonials collections

--- application/views/_partial/collection/testimonial-layout-1.php
+++ application/views/_partial/collection/testimonial-layout-1.php
@@ -9,50 +9,44 @@ if (isset($collection['testimonials']) && count($collection['testimonials']) > 0
                 </h2>
             </div>
         </div>
-        <!-- Slider -->
+
         <div
             class="<?php echo (3 < count($collection['testimonials'])) ? 'js-slider-testimonials' : '';?> slider-testimonials">
             <?php foreach ($collection['testimonials'] as $testimonial) { ?>
-            <div class="slide-item">
-                <div class="slide-item__text">
-                    <p>
-                        <?php echo CommonHelper::truncateCharacters($testimonial['testimonial_text'], 250, '', '', true); ?>
-                        <?php if (strlen($testimonial['testimonial_text']) > 150) {
+            <div class="slide">
+                <div class="slide-item">
+                    <div class="slide-item__text">
+                        <p>
+                            <?php echo CommonHelper::truncateCharacters($testimonial['testimonial_text'], 250, '', '', true); ?>
+                            <?php if (strlen($testimonial['testimonial_text']) > 150) {
                                     echo '...';
                                 } ?>
-                        <?php /* <span class="lessText">
-                            <?php echo CommonHelper::truncateCharacters($testimonial['testimonial_text'], 150, '', '', true); ?>
-                        </span>
-                        <?php if (strlen($testimonial['testimonial_text']) > 150) { ?>
-                        <span class="moreText hidden">
-                            <?php echo FatUtility::decodeHtmlEntities($testimonial['testimonial_text']); ?>
-                        </span>
-                        <br>
-                        <a class="readMore link--arrow btn-link" href="javascript:void(0);">
-                            <?php echo Labels::getLabel('Lbl_SHOW_MORE', $siteLangId) ; ?>
-                        </a>
-                        <?php }  */ ?>
-                    </p>
-                </div>
-                <div class="slide-item__from">
-                    <img class="user-pic" alt="<?php echo $testimonial['testimonial_user_name']; ?>"
-                        src="<?php echo UrlHelper::generateFileUrl('Image', 'testimonial', array($testimonial['testimonial_id'], $siteLangId, 'THUMB')) . '?t=' . time(); ?>">
-                    <div class="user-detail">
-                        <p><span class="name"><?php echo $testimonial['testimonial_user_name']; ?></span>
-                            <!--span class="designation">Senior UI/UX Designer</span-->
+
                         </p>
+                    </div>
+                    <div class="slide-item__from">
+                        <img class="user-pic" alt="<?php echo $testimonial['testimonial_user_name']; ?>"
+                            src="<?php echo UrlHelper::generateFileUrl('Image', 'testimonial', array($testimonial['testimonial_id'], $siteLangId, 'THUMB')) . '?t=' . time(); ?>">
+                        <div class="user-detail">
+                            <p>
+                                <span class="name"><?php echo $testimonial['testimonial_user_name']; ?></span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <?php } ?>
+
         </div>
-    </div>
-    <?php } ?>
-    </div>
-    <!-- /Slider -->
-    <div class="section-foot text-center">
-        <a class="btn btn-outline-brand btn-wide"
-            href="<?php echo UrlHelper::generateUrl('Testimonials'); ?>"><?php echo Labels::getLabel('LBL_View_all', $siteLangId); ?></a>
-    </div>
+
+
+
+
+        <div class="section-foot text-center">
+            <a class="btn btn-outline-brand btn-wide"
+                href="<?php echo UrlHelper::generateUrl('Testimonials'); ?>"><?php echo Labels::getLabel('LBL_View_all', $siteLangId); ?>
+            </a>
+        </div>
     </div>
 </section>
 <?php if (3 < count($collection['testimonials'])) { ?>
