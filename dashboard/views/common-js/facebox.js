008f6c243945afb36a3bc8e1c6baa26a1abdc3bd
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-08-16 15:01:18 +0530

    task-87972-Modal body class

--- dashboard/views/common-js/facebox.js
+++ dashboard/views/common-js/facebox.js
@@ -13,7 +13,7 @@
 	$.extend($.facebox, {
 		element: Date.now(),
 		reveal: function (data, bodyClass) {
-            if (0 == $(data).find('modal-body').length && false === $(data).hasClass('modal-body')) {
+            if (0 == $(data).find('.modal-body').length && false === $(data).hasClass('modal-body')) {
                 data = '<div class="modal-body">' + data + '</div>'
             }
 
