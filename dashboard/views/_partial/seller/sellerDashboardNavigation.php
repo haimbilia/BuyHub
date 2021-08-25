05af314ae069b00c7f4278d37e8ed54d8ee2016a
Maninder Singh <maninder.singh@fatbit.in>
2021-08-25 17:26:22 +0530

    task-85799 seller dashboard icon updates

--- dashboard/views/_partial/seller/sellerDashboardNavigation.php
+++ dashboard/views/_partial/seller/sellerDashboardNavigation.php
@@ -580,12 +580,9 @@ $plugin = new Plugin();
                         <li class="menu__item <?php echo ($controller == 'sellerplugins' && $action == 'index' && is_array($params) && current($params) == $type) ? 'is-active' : ''; ?>">
                             <div class="menu__item__inner">
                                 <a title="<?php echo $name; ?>" href="<?php echo UrlHelper::generateUrl('sellerPlugins', 'index', [$type]); ?>">
-                                    <i class="icn shop"><svg class="svg">
-<<<<<<< HEAD
-                                        <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#<?php echo $name; ?>"></use>
-=======
-                                        <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#<?php echo PluginCommon::getPluginTypeIconClass($type); ?>"></use>
->>>>>>> a922623f0edbc4d3bba0afdec5758a0a0c47ea8c
+                                    <i class="icn shop">
+                                        <svg class="svg">
+                                            <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#<?php echo PluginCommon::getPluginTypeIconClass($type); ?>"></use>
                                         </svg>
                                     </i>
                                     <span class="menu-item__title"><?php echo $name; ?></span>
