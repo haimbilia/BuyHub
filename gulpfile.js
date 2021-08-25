98dd1e2c9977f4bac7cf66bb3cd7add7aec04931
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-08-25 10:56:24 +0530

    task-88921 continued working on POC html

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,16 @@ function css() {
     .pipe(sourcemaps.write("."))
     .pipe(dest("./application/views/css"));
 }
+
+function manager() {
+  return src("./manager/views/scss/*.scss")
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./manager/views/css"));
+}
+
 function dashboard() {
   return src("./dashboard/views/scss/*.scss")
     .pipe(sourcemaps.init({ loadMaps: true }))
@@ -26,7 +36,8 @@ function dashboard() {
 function watchFiles() {
   watch(["./application/views/scss"], css);
   watch(["./dashboard/views/scss"], dashboard);
+  watch(["./manager/views/scss"], manager);
 }
 
-exports.default = series(css, dashboard);
+exports.default = series(css, dashboard, manager);
 exports.watch = watchFiles;
