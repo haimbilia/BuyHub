f3d3e6dc593318e290255b8a4df98a2bf041e2d5
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-09-04 15:41:02 +0530

    task-88921 worked on Login/Forgot Password pages

--- manager/views/design/login-1.php
+++ manager/views/design/login-1.php
@@ -1,67 +1,52 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="light" dir="ltr">
 
-
     <head>
         <meta charset="utf-8" />
         <title>FATbit | Dashboard</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
             rel="stylesheet">
-        <script>
-        WebFont.load({
-            google: {
-                "families": ["Poppins:300,400,500,600,700"]
-            },
-            active: function() {
-                sessionStorage.fonts = true;
-            }
-        });
-        </script>
-
         <link href="<?php echo CSS_PATH;?>main-ltr.css" rel="stylesheet" type="text/css" />
-
         <link rel="shortcut icon" href="images/favicon.ico" />
     </head>
 
-
-    <body class="is-login  login-1">
+    <body>
         <div id="particles-js"></div>
-        <div class="login-page">
+        <div class="login-page login-1">
             <div class="container">
                 <div class="row align-item-center justify-content-center">
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-head">
-                                <figure class="logo text-center p-4 mx-auto">
-                                    <a href="index.php"><img title="Yo!Kart"
+                                <div class="logo">
+                                    <a href="index.php">
+                                        <img title="Yo!Kart"
                                             src="<?php echo CONF_WEBROOT_URL;?>images/logos/logo-coloured.png"
-                                            alt="Yo!Kart"></a>
-                                </figure>
+                                            alt="Yo!Kart">
+                                    </a>
+                                </div>
+
                             </div>
-                            <div class="card-body p-5">
-                                <h2>Sign In to Yokart</h2>
+                            <div class="card-body">
+                                <div class="title">
+                                    <h2>Sign In to Yokart</h2>
+                                </div>
                                 <form class="form">
                                     <div class="form-group"><input class="form-control" title="Username"
                                             placeholder="Username" type="text" value="">
                                         <input class="form-control" title="Password" placeholder="Password"
                                             type="password" value="">
                                     </div>
-                                    <div class="row pt-3 pb-3">
-                                        <div class="col-6">
-                                            <div class="field-set ">
-                                                <label class="switch switch--sm remember-me">
-                                                    <input type="checkbox" name="">
-                                                    <span></span>Remember Me </label>
-                                            </div>
+                                    <div class="row py-3">
+                                        <div class="col-12">
+                                            <label class="switch switch--sm remember-me">
+                                                <input type="checkbox" name="">
+                                                <span></span>Remember Me </label>
+
                                         </div>
-                                        <div class="col-6">
-                                            <div class="field-set text-right">
-                                                <a href="" class="link">Forgot Password?</a>
-                                            </div>
-                                        </div>
+
                                     </div>
                                     <div class="row">
                                         <div class="col-md-12">
@@ -71,14 +56,19 @@
                                     </div>
                                 </form>
                             </div>
+                            <div class="card-foot">
+                                <ul class="other-links">
+                                    <li><a href="#" class="">Forgot Password?</a></li>
+
+                                </ul>
+                            </div>
                         </div>
-                        <p class="version text-white text-center mt-5"><strong>Admin version 2019</strong></p>
+                        <p class="version">Admin version 2019</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <script src="js/vendors/particles.min.js"></script>
-        <script src="js/vendors/script.js"></script>
+            <script src="js/vendors/particles.min.js"></script>
+            <script src="js/vendors/script.js"></script>
     </body>
 
 </html>
