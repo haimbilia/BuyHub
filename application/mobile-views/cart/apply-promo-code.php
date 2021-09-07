61a23b229ba6f26c1d4575c94e1c981e1223e5b2
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-09-07 17:14:04 +0530

    task-85717-remove unwanted code from apply promo code api

--- application/mobile-views/cart/apply-promo-code.php
+++ application/mobile-views/cart/apply-promo-code.php
@@ -1,9 +1,2 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
-
-$productsArr = [
-    'notAvailable' => [],
-    'available' => [],
-    'saveForLater' => [],
-];
-
 require_once(CONF_THEME_PATH . 'cart/price-detail.php');
