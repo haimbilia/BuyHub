f09c6643aea545e36e60f29e61df36b03342a4b9
ablyanup <anup.rawat@ablysoft.com>
2021-09-14 16:48:47 +0530

    task-89461 listing page setup

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -394,17 +394,26 @@ class HomeController extends MyAppController
         $this->_template->render(false, false, 'json-success.php');
     }
 
+        
+    /**
+     * currencies : Used for APPs
+     *
+     * @return void
+     */
     public function currencies()
     {
+        $defaultCurrencyId = FatApp::getConfig('CONF_CURRENCY', FatUtility::VAR_INT, 1);
         $cObj = Currency::getSearchObject($this->siteLangId, true);
         $cObj->addMultipleFields(
             array(
-                'currency_id', 'currency_code', 'IFNULL(curr_l.currency_name,curr.currency_code) as currency_name'
+                'currency_id',
+                'currency_code',
+                'IFNULL(curr_l.currency_name,curr.currency_code) as currency_name',
+                'IF(currency_id = ' . $defaultCurrencyId . ', 1, 0) as isDefault'
             )
         );
-        $rs = $cObj->getResultSet();
-        $currencies = $this->db->fetchAll($rs);
-        $this->set('currencies', $currencies);
+        $currencies = $this->db->fetchAll($cObj->getResultSet());
+        $this->set('data', ['currencies' => $currencies]);
         $this->_template->render();
     }
 
@@ -1308,9 +1317,7 @@ class HomeController extends MyAppController
         $productSrchSponObj->addFld('IFNULL(prod_rating, 0) as prod_rating');
         $productSrchSponObj->setPageNumber($page);
         $productSrchSponObj->setPageSize($pagesize);
-
-        $rs = $productSrchSponObj->getResultSet();
-        $sponsoredProds = FatApp::getDb()->fetchAll($rs);
+        $sponsoredProds = FatApp::getDb()->fetchAll($productSrchSponObj->getResultSet());
 
         $this->set('sponsoredProds', $sponsoredProds);
         $this->set('page', $page);
@@ -1363,20 +1370,26 @@ class HomeController extends MyAppController
         $this->set('image_url', $image_url);
         $this->_template->render();
     }
-
+    
+    /**
+     * countries : Used for APPs
+     *
+     * @return void
+     */
     public function countries()
     {
         $countryObj = new Countries();
         $countriesArr = $countryObj->getCountriesArr($this->siteLangId);
-        $arr_country = array();
+
+        $arrCountry = array();
         foreach ($countriesArr as $country) {
-            $arr_country[] = [
+            $arrCountry[] = [
                 "id" => $country['country_id'],
                 'name' => $country['country_name'],
                 'country_code' => $country['country_code'],
             ];
         }
-        $this->set('countries', $arr_country);
+        $this->set('data', ['countries' => $arrCountry]);
         $this->_template->render();
     }
 
--- dashboard/mobile-views/buyer/view-order-return-request.php
+++ dashboard/mobile-views/buyer/view-order-return-request.php
@@ -3,7 +3,7 @@
 $returnDataArr = CommonHelper::getOrderProductRefundAmtArr($request);
 $request['op_refund_amount'] = CommonHelper::displayMoneyFormat($returnDataArr['op_refund_amount'], true, false);
 
-$request['charges'] = array_key_exists('charges', $request) ? array_values($request['charges']) : array();
+$request['charges'] = array_key_exists('charges', $request) && is_array($request['charges']) ? array_values($request['charges']) : array();
 $request['orRequestTypeTitle'] = !empty($returnRequestTypeArr[$request['orrequest_type']]) ? $returnRequestTypeArr[$request['orrequest_type']] : '';
 $request['orRequestStatusTitle'] = !empty($requestRequestStatusArr[$request['orrequest_status']]) ? $requestRequestStatusArr[$request['orrequest_status']] : '';
 
