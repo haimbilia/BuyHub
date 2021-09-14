7e2e40d85a8c28e2bebdf7822b9e5e84bf4385ed
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-09-14 10:07:39 +0530

    task-81594-buyer order return request search

--- dashboard/mobile-views/buyer/order-return-request-search.php
+++ dashboard/mobile-views/buyer/order-return-request-search.php
@@ -1,6 +1,6 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
-foreach ($requests as $key => &$request) {
+foreach ($requests as &$request) {
     $request['statusName'] = array_key_exists('orrequest_status', $request) ? $OrderReturnRequestStatusArr[$request['orrequest_status']] : '';
     $request['orrequestTypeTitle'] = array_key_exists('orrequest_type', $request) ? $returnRequestTypeArr[$request['orrequest_type']] : '';
     $request['product_image_url'] = UrlHelper::generateFullUrl('image', 'product', array($request['selprod_product_id'], "THUMB", $request['op_selprod_id'], 0, $siteLangId), CONF_WEBROOT_FRONTEND);
