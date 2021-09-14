f97031c0a415a511091913310db6beec3297e3fb
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-09-14 10:02:59 +0530

    task-81594-buyer order cancellation request search

--- dashboard/mobile-views/buyer/order-cancellation-request-search.php
+++ dashboard/mobile-views/buyer/order-cancellation-request-search.php
@@ -1,8 +1,8 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
-foreach ($requests as $key => $request) {
-    $requests[$key]['statusName'] = array_key_exists('ocrequest_status', $request) ? $OrderCancelRequestStatusArr[$request['ocrequest_status']] : '';
-    $requests[$key]['product_image_url'] = UrlHelper::generateFullUrl('image', 'product', array($request['selprod_product_id'], "THUMB", $request['op_selprod_id'], 0, $siteLangId), CONF_WEBROOT_FRONTEND);
+foreach ($requests as $key => &$request) {
+    $request['statusName'] = array_key_exists('ocrequest_status', $request) ? $OrderCancelRequestStatusArr[$request['ocrequest_status']] : '';
+    $request['product_image_url'] = UrlHelper::generateFullUrl('image', 'product', array($request['selprod_product_id'], "THUMB", $request['op_selprod_id'], 0, $siteLangId), CONF_WEBROOT_FRONTEND);
 }
 
 $data = array(
