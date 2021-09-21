fef6015e6d9608e335109bea7fe43837709fadb1
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-09-21 09:23:53 +0530

    task-88921-Sort Icon change on selection

--- manager-application/utilities/HtmlHelper.php
+++ manager-application/utilities/HtmlHelper.php
@@ -4,33 +4,29 @@ class HtmlHelper
     public static function getLoader()
     {
         return '<div class="table-processing">
-        <div class="spinner spinner--sm spinner--brand"></div>
-    </div>';
+                    <div class="spinner spinner--sm spinner--brand"></div>
+                </div>';
     }
 
     public static function getListingHeaderColumnHtml($key, $sortBy, $sortOrder)
     {
         if ($key == $sortBy) {
+            $class = 'sorting_desc';
+            $selectorId = '#arrow-up';
+
             if ($sortOrder == applicationConstants::SORT_ASC) {
-                return [
-                    'class' => 'sorting_asc',
-                    'html' => '<i class="icn">
-                    <svg class="svg" width="18" height="18">
-                        <use xlink:href="' . CONF_WEBROOT_URL . 'images/retina/sprite-actions.svg#arrow-down">
-                        </use>
-                    </svg>
-                </i>'
-                ];
+                $class = 'sorting_asc';
+                $selectorId = '#arrow-down';
             }
 
             return [
-                'class' => 'sorting_desc',
-                'html' => '<i class="icn">
-                <svg class="svg" width="18" height="18">
-                    <use xlink:href="' . CONF_WEBROOT_URL . 'images/retina/sprite-actions.svg#arrow-up">
-                    </use>
-                </svg>
-            </i>'
+                'class' => $class,
+                'html' => '<i class="icn sortingIconJs">
+                                <svg class="svg" width="18" height="18">
+                                    <use xlink:href="' . CONF_WEBROOT_URL . 'images/retina/sprite-actions.svg' . $selectorId . '">
+                                    </use>
+                                </svg>
+                            </i>'
             ];
         }
 
