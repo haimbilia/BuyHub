28236d0bcc9000be0479245aa6d1030f3c0e8de4
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-10-13 16:40:31 +0530

    task-90831-Configuration general settings form design

--- manager-application/views/footer.php
+++ manager-application/views/footer.php
@@ -6,6 +6,11 @@
 echo $this->getJsCssIncludeHtml(!CONF_DEVELOPMENT_MODE, false, true, false, false); ?>
 </div>
 </div>
+
+<?php if (!isset($_SESSION['geo_location']) && FatApp::getConfig('CONF_GOOGLEMAP_API_KEY', FatUtility::VAR_STRING, '') != '') { ?>
+    <script type="text/javascript" src='https://maps.google.com/maps/api/js?key=<?php echo FatApp::getConfig('CONF_GOOGLEMAP_API_KEY', FatUtility::VAR_STRING, '');?>&libraries=places'></script>
+<?php } ?>
+
 </body>
 
 </html>
