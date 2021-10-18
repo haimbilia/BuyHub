8f8b0a33b8d14d558616b497243bee9419b202ec
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-10-18 13:56:52 +0530

    task-90879-1721210-3- Language translate icon ui updated

--- manager-application/views/_partial/listing/lang-form.php
+++ manager-application/views/_partial/listing/lang-form.php
@@ -8,22 +8,21 @@ $langFrm->setFormTagAttribute('onsubmit', 'saveLangData(this); return(false);');
 
 $langFld = $langFrm->getField('lang_id');
 $langFld->setfieldTagAttribute('onChange', "editLangData(" . $recordId . ", this.value);");
+$translatorSubscriptionKey = FatApp::getConfig('CONF_TRANSLATOR_SUBSCRIPTION_KEY', FatUtility::VAR_STRING, '');
+if (!empty($translatorSubscriptionKey)) {
+    $langFld->developerTags['fldWidthValues'] = ['d-flex', '', '', ''];
+    $langFld->htmlAfterField = '<a href="javascript:void(0);" onclick="editLangData(' . $recordId . ', ' . $lang_id . ', 1)" class="btn" title="' .  Labels::getLabel('LBL_AUTOFILL_LANGUAGE_DATA', $siteLangId) . '">
+                                <svg class="svg" width="18" height="18">
+                                    <use xlink:href="' . CONF_WEBROOT_URL . 'images/retina/sprite.yokart.svg#icon-translate">
+                                    </use>
+                                </svg>
+                            </a>';
+}
 
 $activeLangtab = true;
 require_once(CONF_THEME_PATH . '_partial/listing/form-head.php'); ?>
     <div class="form-edit-body loaderContainerJs">
-        <?php
-        $translatorSubscriptionKey = FatApp::getConfig('CONF_TRANSLATOR_SUBSCRIPTION_KEY', FatUtility::VAR_STRING, '');
-        if (!empty($translatorSubscriptionKey)) {
-            $langFld->developerTags['fldWidthValues'] = ['d-flex', '', '', ''];
-            $langFld->htmlAfterField = '<a href="javascript:void(0);" onclick="editLangData(' . $recordId . ', ' . $lang_id . ', 1)" class="btn" title="' .  Labels::getLabel('LBL_AUTOFILL_LANGUAGE_DATA', $siteLangId) . '">
-                                        <svg class="svg" width="18" height="18">
-                                            <use xlink:href="' . CONF_WEBROOT_URL . 'images/retina/sprite.yokart.svg#icon-translate">
-                                            </use>
-                                        </svg>
-                                    </a>';
-        }
-        echo $langFrm->getFormHtml(); ?>
+        <?php echo $langFrm->getFormHtml(); ?>
     </div>
     <?php require_once(CONF_THEME_PATH . '_partial/listing/form-edit-foot.php'); ?>
 </div> <!-- Close </div> This must be placed. Opening tag is inside form-head.php file. -->
