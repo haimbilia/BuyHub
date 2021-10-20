3a74c47e5472419f68cba72b7aa8d916009255b6
ablyshashi <shashi.kumar@fatbit.in>
2021-10-20 10:06:35 +0530

    task-90520-working on tax category listing

---
+++ manager-application/views/tax/index.php
@@ -0,0 +1,67 @@
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
+$keywordPlaceholder = Labels::getLabel('FRM_SEARCH_BY_CATEGORY_NAME', $siteLangId);
+
+/* No sorting functionality required if no record found. */
+if (2 > count($arrListing)) {
+    $allowedKeysForSorting = [];
+}
+
+$tableHeadAttrArr = [
+    'select_all' => [
+        'width' => '5%'
+    ],
+    'listSerial' => [
+        'width' => '10%'
+    ],
+    'taxcat_identifier' => [
+        'width' => '25%'
+    ], 
+    'taxcat_active' => [
+        'width' => '10%'
+    ],
+    'action' => [
+        'width' => '10%'
+    ],
+];
+
+$controller = str_replace('Controller', '', FatApp::getController());
+?>
+<main class="main mainJs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php require_once(CONF_THEME_PATH . '_partial/listing/listing-search-form.php'); ?>
+                <div class="card">
+                    <?php $data = [
+                        'canEdit' => $canEdit,
+                        'siteLangId' => $siteLangId,
+                        'cardHeadTitle' => Labels::getLabel('LBL_EMPTY_CART_ITEMS', $siteLangId),
+                        'newRecordBtn' => true,
+                        'statusButtons' => true,
+                        'deleteButton' => true
+                    ];
+                    $this->includeTemplate('_partial/listing/listing-head.php', $data, false); ?>
+                    <div class="card-body">
+                        <div class="table-responsive listingTableJs">
+                            <?php
+                            require_once(CONF_THEME_PATH . '_partial/listing/listing-column-head.php');
+                            require_once(CONF_THEME_PATH . 'tax/search.php');
+
+                            $data = [
+                                'tbl' => $tbl, /* Received from listing-column-head.php file. */
+                                'controller' => $controller /* Used in case of performing bulk action. */
+                            ];
+                            $this->includeTemplate('_partial/listing/print-listing-table.php', $data, false); ?>
+                        </div>
+                    </div>
+                    <?php require_once(CONF_THEME_PATH . '_partial/listing/listing-foot.php'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script>
+    var controllerName = '<?php echo $controller; ?>';
+    getHelpCenterContent(controllerName);
+</script>
