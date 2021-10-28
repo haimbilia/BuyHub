9b5c12d85d4026cb2f13ba5151742ef0acbb6d21
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2021-10-28 17:15:51 +0530

    task-90993 worked UI recommendations

--- manager/views/design/add-products.php
+++ manager/views/design/add-products.php
@@ -134,13 +134,152 @@
                                 <div class="card">
                                     <div class="card-head">
                                         <div class="card-head-label">
-                                            <h3 class="card-head-title">Basic Details
-                                            </h3>
+                                            <h3 class="card-head-title">Basic Details </h3>
                                             <span class="text-muted"> Add basic details about your product</span>
                                         </div>
                                     </div>
-                                    <div class="card-body">Body</div>
-                                    <div class="card-foot">Foot</div>
+                                    <div class="card-body">
+                                        <form class="form" action="">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="label required"> Product type </label>
+                                                        <div class="">
+                                                            <ul class="list-radio">
+                                                                <li>
+                                                                    <label class="radio">
+                                                                        <input type="radio" value="0" checked="checked">
+                                                                        Physical</label>
+                                                                </li>
+                                                                <li><label class="radio">
+                                                                        <input type="radio" value="0">
+                                                                        Digital</label>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="label required">
+                                                            Product name
+                                                            <i class="fas fa-exclamation-circle ms-2 fs-7"
+                                                                data-toggle="tooltip" title=""
+                                                                data-original-title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                                                                aria-label="Lorem ipsum dolor sit amet consectetur adipisicing elit"></i>
+                                                        </label>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <div class="d-flex justify-content-between">
+                                                            <label class="label required "> Brand </label> <a
+                                                                class="link" href="">Add brand</a>
+                                                        </div>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <div class="d-flex justify-content-between">
+                                                            <label class="label required ">Category </label> <a
+                                                                class="link" href="">Add category</a>
+                                                        </div>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <div class="d-flex justify-content-between">
+                                                            <label class="label required "> Brand </label> <a
+                                                                class="link" href="">Add brand</a>
+                                                        </div>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <div class="d-flex justify-content-between">
+                                                            <label class="label required ">Category </label> <a
+                                                                class="link" href="">Add category</a>
+                                                        </div>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+
+
+                                        </form>
+
+                                    </div>
+
+                                </div>
+                                <div class="card">
+                                    <div class="card-head dropdown-toggle-custom collapsed" data-toggle="collapse"
+                                        data-target="#stock-block1" aria-expanded="false" aria-controls="stock-block1">
+                                        <div class="card-head-label">
+                                            <h3 class="card-head-title">Basic Details
+                                            </h3>
+                                            <span class="text-muted"> Add basic details about your product</span>
+                                        </div> <i class="dropdown-toggle-custom-arrow"></i>
+                                    </div>
+                                    <div class="card-body collapse" id="stock-block1">
+                                        <form class="form" action="">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="label">
+                                                            <span class="required">Product type
+                                                            </span>
+
+                                                        </label>
+                                                        <div class="">
+                                                            <ul class="list-radio">
+                                                                <li>
+                                                                    <label class="radio">
+                                                                        <input type="radio" value="0" checked="checked">
+                                                                        Physical</label>
+                                                                </li>
+                                                                <li><label class="radio">
+                                                                        <input type="radio" value="0">
+                                                                        Digital</label>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label class="label">
+                                                            <span class="required">Product name</span>
+                                                            <i class="fas fa-exclamation-circle ms-2 fs-7"
+                                                                data-toggle="tooltip" title=""
+                                                                data-original-title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+                                                                aria-label="Lorem ipsum dolor sit amet consectetur adipisicing elit"></i>
+                                                        </label>
+                                                        <input type="text" placeholder="">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </form>
+
+                                    </div>
+
                                 </div>
                             </div>
                             <div class="add-stock-column">
