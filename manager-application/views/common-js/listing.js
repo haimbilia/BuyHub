11742da0cb63204f236b6739f8ee74f254413ffd
Satbir Kaushik <satbir.kaushik@fatbit.in>
2021-11-08 13:26:32 +0530

    task-91534-CRUD functionality having media tabs

--- manager-application/views/common-js/listing.js
+++ manager-application/views/common-js/listing.js
@@ -714,6 +714,10 @@ $(document).on("hidden.bs.modal", "#modalBoxJs", function () {
         $.ykmodal.close();
     }
 
+    editDropZoneImages = function () {
+        $(".dropzoneContainerJs .dropzoneInputJs").click();
+    }
+
     // convertCheckboxToSwitch = function () {
     //   if (0 < $("form.checkboxSwitchJs").length) {
     //     $("form.checkboxSwitchJs")
