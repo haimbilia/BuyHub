e0ad668ae493e298fe9c14a5d4c3e76e5cbef8ad
ablyshashi <shashi.kumar@fatbit.in>
2021-11-29 17:26:26 +0530

    task-92309-handled tag with language

--- manager-application/views/tags/search.php
+++ manager-application/views/tags/search.php
@@ -10,22 +10,17 @@ $serialNo = $page == 1 ? 0 : $pageSize * ($page - 1);
 foreach ($arrListing as $selProdId => $row) {
     $serialNo++;
     $cls = (($serialNo % 2) == 0) ? 'even' : 'odd';
-    $tr = $tbody->appendElement('tr', ['class' => $cls, 'data-row' => $serialNo]); 
+    $tr = $tbody->appendElement('tr', ['class' => $cls, 'data-row' => $serialNo]);
     foreach ($fields as $key => $val) {
         $td = $tr->appendElement('td');
         switch ($key) {
             case 'listSerial':
                 $td->appendElement('plaintext', [], $serialNo);
-                break; 
+                break;
             case 'tags':
-                $productTags = Product::getProductTags($row['product_id'],$siteLangId);
-                $tagData = array();
-                foreach ($productTags as $key => $data) {
-                    $tagData[$key]['id'] = $data['tag_id'];
-                    $tagData[$key]['value'] = $data['tag_name'];
-                }
-                $encodedData = htmlspecialchars(json_encode($tagData), ENT_QUOTES, 'UTF-8');
-                $td->appendElement('plaintext', array(), "<div class='product-tag' id='product" . $row['product_id'] . "'><input class='productsTagsJs' type='text' name='tag_name" . $row['product_id'] . "' value='" . $encodedData . "' data-product_id='" . $row['product_id'] . "'></div>", true);
+                $productTags = Product::getProductTags($row['product_id'], $langId, false, array('tag_id as id', 'tag_name as value'));
+                $encodedData = htmlspecialchars(json_encode($productTags), ENT_QUOTES, 'UTF-8');
+                $td->appendElement('plaintext', array(), "<div class='product-tag' id='product" . $row['product_id'] . "'><input class='productsTagsJs'  type='text' name='tag_name" . $row['product_id'] . "' value='" . $encodedData . "' data-product_id='" . $row['product_id'] . "'></div>", true);
                 break;
             default:
                 $td->appendElement('plaintext', '', $row[$key], true);
@@ -36,12 +31,12 @@ foreach ($arrListing as $selProdId => $row) {
 
 if (count($arrListing) == 0) {
     $tbody->appendElement('tr')->appendElement(
-            'td',
-            array(
-                'colspan' => count($fields),
-                'class' => 'noRecordFoundJs'
-            ),
-            Labels::getLabel('LBL_NO_RECORDS_FOUND', $siteLangId)
+        'td',
+        array(
+            'colspan' => count($fields),
+            'class' => 'noRecordFoundJs'
+        ),
+        Labels::getLabel('LBL_NO_RECORDS_FOUND', $langId)
     );
 }
 
@@ -49,10 +44,8 @@ if ($printData) {
     echo $tbody->getHtml();
 }
 ?>
-<script>  
-$(function() {
-    TagTagify();
-});
-
-</script>    
- 
+<script>
+    $(function() {
+        TagTagify('<?php echo $formLayout; ?>');
+    });
+</script>
