674d5657c6ee88a4123e17d1980a8314f2748846
Anuj Sharma <anuj.sharma@fatbit.in>
2021-12-02 12:23:18 +0530

    task-92402: Removed teh Automatic Translate form field from Language form section

--- manager-application/controllers/SlidesController.php
+++ manager-application/controllers/SlidesController.php
@@ -291,12 +291,6 @@ class SlidesController extends ListingBaseController
         $frm->addHiddenField('', 'slide_id');
         $frm->addSelectBox(Labels::getLabel('FRM_LANGUAGE', $langId), 'lang_id', Language::getDropDownList(CommonHelper::getDefaultFormLangId()), $langId, array(), '');
         $frm->addRequiredField(Labels::getLabel('FRM_SLIDE_TITLE', $langId), 'slide_title');
-        $siteLangId = FatApp::getConfig('conf_default_site_lang', FatUtility::VAR_INT, 1);
-        $translatorSubscriptionKey = FatApp::getConfig('CONF_TRANSLATOR_SUBSCRIPTION_KEY', FatUtility::VAR_STRING, '');
-        if (!empty($translatorSubscriptionKey) && $langId == $siteLangId) {
-            $frm->addCheckBox(Labels::getLabel('FRM_UPDATE_OTHER_LANGUAGES_DATA', $langId), 'auto_update_other_langs_data', 1, array(), false, 0);
-        }
-
         return $frm;
     }
 
