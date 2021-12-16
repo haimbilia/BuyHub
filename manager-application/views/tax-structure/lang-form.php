2655fdd8b2549f698bca8f76db0995a9c3660d2f
mohit <mohit.kumar@maxixx.com>
2021-12-16 16:41:52 +0530

    task-88921-admin-ui-ux form modification

--- manager-application/views/tax-structure/lang-form.php
+++ manager-application/views/tax-structure/lang-form.php
@@ -3,7 +3,9 @@
 defined('SYSTEM_INIT') or die('Invalid Usage.');
 
 $otherButtons = [
-];
+]; 
+$displayObj = $langFrm->getField('taxstr_component_name[]');
+$displayObj->developerTags['noCaptionTag'] = true;
 $formTitle = Labels::getLabel('LBL_TAX_STRUCTURE_SETUP', $siteLangId);
 require_once(CONF_THEME_PATH . '_partial/listing/lang-form.php');
 ?>
