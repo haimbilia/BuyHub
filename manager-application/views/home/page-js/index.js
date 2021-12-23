a0541beeaf6fb4ce5896c4ea393efc4b975c0c07
ablyanup <anup.rawat@ablysoft.com>
2021-12-23 17:57:42 +0530

    task-92776 dashboard stats

--- manager-application/views/home/page-js/index.js
+++ manager-application/views/home/page-js/index.js
@@ -233,11 +233,8 @@ $(window).on('load', function () {
 	topSellingProducts();
 	getTopSearchKeyword('yearly');
 	traficSource('yearly');
-	/* visitorStats();
-	
+	visitorStats();
 
-	
-	 */
 	// $('.carousel--oneforth-js').slick(getSlickSliderSettings(4));
 	/* FUNCTION FOR SCROLLBAR */
 	/* $('.scrollbar-js').enscroll({
