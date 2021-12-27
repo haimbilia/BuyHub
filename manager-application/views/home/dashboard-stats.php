464825c81266c99e84aebf991366e1ed087591b6
ablyanup <anup.rawat@ablysoft.com>
2021-12-27 14:22:27 +0530

    task-92776 dashboard updates

--- manager-application/views/home/dashboard-stats.php
+++ manager-application/views/home/dashboard-stats.php
@@ -1,32 +1,32 @@
 <?php
 switch (strtoupper($stats_type)) {
     case 'TOP_COUNTRIES':
-        if ($stats_info == null || !array_key_exists('totalsForAllResults', $stats_info) || $stats_info['totalsForAllResults'] == 0) {
+        if (null != $stats_info && array_key_exists('rows', $stats_info) && $stats_info['totalsForAllResults'] > 0) {
+            foreach ($stats_info['rows'] as $key => $val) {
+                $cls = (1 >  $val['%age']) ? 'fa-arrow-down font-danger' : 'fa-arrow-up font-success';
+                echo '<li class="list-stats-item">
+                        <span class="label">' . $key . '</span>
+                        <span class="value">
+                            <i class="icn fas ' . $cls . '"></i>' . $val['%age'] . '%</span>
+                    </li>';
+            }
+        } else {
             echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_NO_RECORD_FOUND', $siteLangId) . "</li>";
-            exit;
         }
 
-        foreach ($stats_info['rows'] as $key => $val) {
-            $cls = (1 >  $val['%age']) ? 'fa-arrow-down font-danger' : 'fa-arrow-up font-success';
-            echo '<li class="list-stats-item">
-                    <span class="label">' . $key . '</span>
-                    <span class="value">
-                        <i class="icn fas ' . $cls . '"></i>' . $val['%age'] . '%</span>
-                </li>';
-        }
         break;
     case 'TOP_REFERRERS':
-        if ($stats_info == null || !array_key_exists('totalsForAllResults', $stats_info) || $stats_info['totalsForAllResults'] == 0) {
-            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
-            exit;
-        }
-        foreach ($stats_info['rows'] as $key => $val) {
-            $cls = (30 <  $val['visit']) ? 'fa-arrow-up font-success' : '';
-            echo '<li class="list-stats-item">
+        if (null != $stats_info && array_key_exists('rows', $stats_info) && $stats_info['totalsForAllResults'] > 0) {
+            foreach ($stats_info['rows'] as $key => $val) {
+                $cls = (30 <  $val['visit']) ? 'fa-arrow-up font-success' : '';
+                echo '<li class="list-stats-item">
                     <span class="label">' . $key . '</span>
                     <span class="value">
                         <i class="icn fas ' . $cls . '"></i>' . $val['visit'] . '%</span>
                 </li>';
+            }
+        } else {
+            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
         }
         break;
     case 'TRAFFIC_SOURCE':
@@ -40,6 +40,8 @@ switch (strtoupper($stats_type)) {
             }
             $pieChatStats = rtrim($pieChatStats, ',');
             echo $pieChatStats .= "],['title','Traffic source']";
+        } else {
+            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
         }
         break;
     case 'VISITORS_STATS':
@@ -61,39 +63,42 @@ switch (strtoupper($stats_type)) {
             }
             $chatStats = rtrim($chatStats, ',');
             echo $chatStats .= "]";
+        } else {
+            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
         }
         break;
     case 'TOP_PRODUCTS':
-        if ($stats_info == null || count($stats_info) == 0) {
-            echo "<li>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
-            exit;
-        }
-        $count = 1;
+        if ($stats_info != null && count($stats_info) > 0) {
+            $count = 1;
 
-        foreach ($stats_info as $row) {
-            if ($count > 11) {
-                break;
+            foreach ($stats_info as $row) {
+                if ($count > 11) {
+                    break;
+                }
+                echo '<li>' . $row['product_name'] . '<span class="count">' . $row['sold'] . ' sold</span></li>';
             }
-            echo '<li>' . $row['product_name'] . '<span class="count">' . $row['sold'] . ' sold</span></li>';
+        } else {
+            echo "<li>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
         }
+
         break;
     case 'TOP_SEARCH_KEYWORD':
-        if ($stats_info == null || count($stats_info) == 0) {
-            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
-            exit;
-        }
-        $count = 1;
-        foreach ($stats_info as $row) {
-            if ($count > 11) {
-                break;
-            }
-            $cls = (30 <  $row['search_count']) ? 'fa-arrow-up font-success' : '';
-            $keyword = ($row['searchitem_keyword'] == '') ? 'Blank Search' : $row['searchitem_keyword'];
-            echo '<li class="list-stats-item">
+        if ($stats_info != null && count($stats_info) > 0) {
+            $count = 1;
+            foreach ($stats_info as $row) {
+                if ($count > 11) {
+                    break;
+                }
+                $cls = (30 <  $row['search_count']) ? 'fa-arrow-up font-success' : '';
+                $keyword = ($row['searchitem_keyword'] == '') ? 'Blank Search' : $row['searchitem_keyword'];
+                echo '<li class="list-stats-item">
                     <span class="label">' . $keyword . '</span>
                     <span class="value">
                         <i class="icn fas ' . $cls . '"></i>' . $row['search_count'] . '%</span>
                 </li>';
+            }
+        } else {
+            echo "<li class='list-stats-item'>" . Labels::getLabel('LBL_No_Record_Found', $siteLangId) . "</li>";
         }
         break;
 }
