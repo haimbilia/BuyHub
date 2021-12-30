29dcf9043510771420a5d622167355045dc38cbd
ablyshashi <shashi.kumar@fatbit.in>
2021-12-30 16:08:08 +0530

    task-93428-worked on shwoing notification and log in header

--- manager-application/views/system-log/notification-list.php
+++ manager-application/views/system-log/notification-list.php
@@ -3,8 +3,7 @@ if (count($arrListing) > 0) {
 	foreach ($arrListing as $key => $row) {
 		$mainDiv = new HtmlElement("div", array("class" => "notification"));
 		$div = $mainDiv->appendElement("div", array("class" => "notification__img"));
-		$div = $div->appendElement("div", array("class" => "icon"));
-		//$div->appendElement("img", ['class' => '', 'href' => UrlHelper::generateFullUrl('Image', 'user', array($row['notification_user_id'], 'MINI', true), CONF_WEBROOT_FRONT_URL)]);
+		$div = $div->appendElement("div", array("class" => "icon"));		
 
 		$div = $mainDiv->appendElement("div", array("class" => "notification__detail"));
 		$div->appendElement("a", ['href' => 'javascript:void(0)', 'onclick' => "redirectfunc(fcom.makeUrl('SystemLog'), {slog_id:" . $row['slog_id'] . "}, 0, false);", 'class' => 'title'], $types[$row['slog_type']]);
