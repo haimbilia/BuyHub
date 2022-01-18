3f2d682725766b2a7e237637d2a0b77c23a61aa0
ablyanup <anup.rawat@ablysoft.com>
2022-01-18 11:22:50 +0530

    task-93086 updated kit structure

--- admin-application/views/shipping-company-users/page-js/orders.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	oPWalletTransactions = function( op_id ){
-		$.facebox( function() {
-			fcom.ajax(fcom.makeUrl('shippingCompanyUsers', 'userWalletTransactions'), '&op_id='+op_id, function(t) {
-				fcom.updateFaceboxContent(t);
-			});
-		});
-	};
-})();
--- admin-application/views/users/auto-complete.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('SYSTEM_INIT') or die('Invalid Usage.');
-$html = new HtmlElement('ul');
-foreach($data as $key=>$value){
-	$li=$html->appendElement('li');
-	$li->appendElement('a',array('href'=>'javascript:fillSuggetion(\''.$value['credential_username'].'\')'), $value['user_name'].' ('.$value['credential_username'].')');	
-}
-echo $html->getHtml();
