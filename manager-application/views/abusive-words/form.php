09f8a916cfe6a271518072eb8467910ba223b318
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-01-25 17:55:52 +0530

    bug-061748 - Admin UI: Rate name is not showing while editing the default rate under the shipping profile section

--- manager-application/views/abusive-words/form.php
+++ manager-application/views/abusive-words/form.php
@@ -8,16 +8,5 @@ $frm->setFormTagAttribute('onsubmit', 'saveRecord(this, "closeForm"); return(fal
 
 $fld = $frm->getField('abusive_lang_id');
 $fld->addFieldTagAttribute('onChange', 'changeFormLayOut(this);');
-?>
-<div class="modal-header">
-    <h5 class="modal-title">
-        <?php echo Labels::getLabel('LBL_ABUSIVE_KEYWORD_SETUP', $siteLangId); ?>
-    </h5>
-</div>
-<div class="modal-body form-edit">
-    <div class="form-edit-body loaderContainerJs">
-        <?php echo $frm->getFormHtml(); ?>
-    </div>
 
-    <?php require_once(CONF_THEME_PATH . '_partial/listing/form-edit-foot.php'); ?>
-</div>
+require_once(CONF_THEME_PATH . '_partial/listing/form.php');
