3fd69b4d2e37b270763ae8b10b4927c40fe9bda2
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-02-01 12:46:43 +0530

    task-90752-Replacement variable text should not display if no data added

--- manager-application/views/page-language-data/lang-form.php
+++ manager-application/views/page-language-data/lang-form.php
@@ -11,28 +11,20 @@ $fld = $langFrm->getField('lang_id');
 $fld->setfieldTagAttribute('onChange', "editLangForm('" . $pLangKey . "', this.value);");
 
 $fld = $langFrm->getField('plang_replacements');
-if (!empty($fld->value)) {
-    $repVarArr = array_filter(explode("<br>", $fld->value));
-}
-
-$repVarHtml = '<label class="label">' . Labels::getLabel('LBL_REPLACEMENT_VARS', $siteLangId) . '</label><ul class="click-to-copy">';
-foreach ($repVarArr as $rVar) {
-    $placeholder =  trim(substr($rVar, 0, (strpos($rVar, "}") + 1)));
-    $repVarHtml .= '<li title="' . Labels::getLabel('LBL_CLICK_TO_COPY', $siteLangId) . '" onclick="copyText(this, true);" data-title="' . $placeholder . '" data-bs-toggle="tooltip" data-placement="top">
+$repVarArr = array_filter(explode("<br>", $fld->value));
+if (!empty($repVarArr)) {
+    $repVarHtml = '<label class="label">' . Labels::getLabel('LBL_REPLACEMENT_VARS', $siteLangId) . '</label><ul class="click-to-copy">';
+    foreach ($repVarArr as $rVar) {
+        $placeholder =  trim(substr($rVar, 0, (strpos($rVar, "}") + 1)));
+        $repVarHtml .= '<li title="' . Labels::getLabel('LBL_CLICK_TO_COPY', $siteLangId) . '" onclick="copyText(this, true);" data-title="' . $placeholder . '" data-bs-toggle="tooltip" data-placement="top">
         <div class="text">' . $rVar . '</div>
     </li>';
+    }
+    $repVarHtml .= '</ul>';
+    $fld->value = $repVarHtml;
+} else {
+    $fld->setfieldTagAttribute('class', "d-none");
 }
-$repVarHtml .= '</ul>';
-$fld->value = $repVarHtml;
-?>
-<div class="modal-header">
-    <h5 class="modal-title">
-        <?php echo Labels::getLabel('LBL_PAGE_LANGUAGE_DATA_UPDATE', $siteLangId); ?>
-    </h5>
-</div>
-<div class="modal-body form-edit">
-    <div class="form-edit-body loaderContainerJs">
-        <?php echo $langFrm->getFormHtml(); ?>
-    </div>
-    <?php require_once(CONF_THEME_PATH . '_partial/listing/form-edit-foot.php'); ?>
-</div>
+
+$pageTitle = Labels::getLabel('LBL_PAGE_LANGUAGE_DATA_UPDATE', $siteLangId);
+require_once(CONF_THEME_PATH . '_partial/listing/lang-form.php');
