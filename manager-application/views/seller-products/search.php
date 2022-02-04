c9629a04946404a240ea4bfc9006cce3eeed0312
anant <anant.kumar.singh@fatbit.in>
2022-02-04 17:40:14 +0530

    bug-062114 fixed delete multiple records functionality is not working on seller inventory page at admin end

--- manager-application/views/seller-products/search.php
+++ manager-application/views/seller-products/search.php
@@ -14,7 +14,7 @@ foreach ($arrListing as $sn => $row) {
         $td = $tr->appendElement('td', $tdAttr);
         switch ($key) {
             case 'select_all':
-                $td->appendElement('plaintext', $tdAttr, '<label class="checkbox"><input class="selectItemJs" type="checkbox" name="brandIds[]" value=' . $row['selprod_id'] . '><i class="input-helper"></i></label>', true);
+                $td->appendElement('plaintext', $tdAttr, '<label class="checkbox"><input class="selectItemJs" type="checkbox" name="selprod_ids[]" value=' . $row['selprod_id'] . '><i class="input-helper"></i></label>', true);
                 break;
             case 'listSerial':
                 $td->appendElement('plaintext', $tdAttr, $serialNo);
