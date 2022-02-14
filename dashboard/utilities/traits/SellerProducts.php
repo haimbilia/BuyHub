09aace27fb090902c4a8fcb6e7782d368d95f749
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-02-14 17:23:16 +0530

    task-94791-Breadcrumb fix

--- dashboard/utilities/traits/SellerProducts.php
+++ dashboard/utilities/traits/SellerProducts.php
@@ -3337,6 +3337,7 @@ trait SellerProducts
             $this->nodes[] = array('title' => ucwords($className), 'href' => UrlHelper::generateUrl($urlController));
             $this->nodes[] = array('title' => $title);
         } else {
+            $action = str_replace('-', ' ', FatUtility::camel2dashed($action));
             $title = CommonHelper::replaceStringData(Labels::getLabel('LBL_{ACTION}', $this->siteLangId), ['{ACTION}' => ucwords($action)]);
             $this->nodes[] = array('title' => ucwords($className), 'href' => UrlHelper::generateUrl($urlController));
             $this->nodes[] = array('title' => $title);
