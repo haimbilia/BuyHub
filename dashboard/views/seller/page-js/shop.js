cdddef9d16633520daf4522fba1ba4026f694ed3
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-02-24 16:27:17 +0530

    task-94791-Return Addresses UI and edit, remove functionality

--- dashboard/views/seller/page-js/shop.js
+++ dashboard/views/seller/page-js/shop.js
@@ -166,7 +166,7 @@ $(document).on("change", ".state", function () {
                 shopLangForm(t.shopId, t.langId);
                 return;
             }
-            returnAddressForm();
+            getReturnAddress();
         });
     };
 
@@ -258,7 +258,7 @@ $(document).on("change", ".state", function () {
         if (scollection_id < 0 || typeof (scollection_id) == "undefined") {
             scollection_id = 0;
         }
-        
+
         $.ykmodal(fcom.getLoader());
         fcom.ajax(fcom.makeUrl('Seller', 'shopCollectionGeneralForm', [scollection_id]), '', function (t) {
             fcom.removeLoader();
@@ -305,7 +305,7 @@ $(document).on("change", ".state", function () {
         if (typeof (langId) == "undefined" || langId < 0) {
             return false;
         }
-        
+
         markPopupTabActive();
         $(dvt).prepend(fcom.getLoader());
         fcom.ajax(fcom.makeUrl('seller', 'shopCollectionLangForm', [scollection_id, langId, autoFillLangData]), '', function (t) {
@@ -319,7 +319,7 @@ $(document).on("change", ".state", function () {
         if (scollection_id < 0 || typeof (scollection_id) == "undefined") {
             return false;
         }
-        
+
         $(dvt).prepend(fcom.getLoader());
         fcom.ajax(fcom.makeUrl('Seller', 'shopCollectionProductLinkFrm', [scollection_id]), '', function (t) {
             fcom.removeLoader();
@@ -434,15 +434,27 @@ $(document).on("change", ".state", function () {
         });
     };
 
-    returnAddressForm = function () {
+    getReturnAddress = function () {
         if (1 > $(mtabId).data('shop_id')) {
             return;
         }
         markMainTabActive();
         $(dv).prepend(fcom.getLoader());
-        fcom.ajax(fcom.makeUrl('Seller', 'returnAddressForm'), '', function (t) {
+        fcom.ajax(fcom.makeUrl('Seller', 'getReturnAddress'), '', function (t) {
             fcom.removeLoader();
             $(dv).html(t);
+        });
+    };
+
+    returnAddressForm = function () {
+        if (1 > $(mtabId).data('shop_id')) {
+            return;
+        }
+        fcom.displayProcessing();
+        fcom.ajax(fcom.makeUrl('Seller', 'returnAddressForm'), '', function (t) {
+            fcom.removeLoader();
+            fcom.closeAlertMessage();
+            $.ykmodal(t);
         });
     };
 
@@ -450,16 +462,17 @@ $(document).on("change", ".state", function () {
         if (!$(frm).validate()) return;
         var data = fcom.frmData(frm);
         fcom.updateWithAjax(fcom.makeUrl('Seller', 'setReturnAddress'), data, function (t) {
+            getReturnAddress();
             returnAddressLangForm(t.langId);
         });
     };
 
     returnAddressLangForm = function (langId, autoFillLangData = 0) {
-        markSubTabActive();
-        $(dvt).prepend(fcom.getLoader());
+        fcom.displayProcessing();
         fcom.ajax(fcom.makeUrl('Seller', 'returnAddressLangForm', [langId, autoFillLangData]), '', function (t) {
             fcom.removeLoader();
-            $(dvt).html(t);
+            fcom.closeAlertMessage();
+            $.ykmodal(t);
         });
     };
 
@@ -467,11 +480,17 @@ $(document).on("change", ".state", function () {
         if (!$(frm).validate()) return;
         var data = fcom.frmData(frm);
         fcom.updateWithAjax(fcom.makeUrl('Seller', 'setReturnAddressLang'), data, function (t) {
-            if (t.langId) {
-                returnAddressLangForm(t.langId);
-            } else {
-                returnAddressForm();
-            }
+            getReturnAddress();
+        });
+    };
+
+    deleteReturnAddress = function (id, type) {
+        var agree = confirm(langLbl.confirmDelete);
+        if (!agree) {
+            return false;
+        }
+        fcom.updateWithAjax(fcom.makeUrl('Seller', 'deleteReturnAddress'), '', function (res) {
+            getReturnAddress();
         });
     };
 
@@ -526,7 +545,7 @@ $(document).on("change", ".state", function () {
         if (scollection_id < 0 || typeof (scollection_id) == "undefined") {
             return false;
         }
-        
+
         $(dvt).prepend(fcom.getLoader());
         fcom.ajax(fcom.makeUrl('Seller', 'shopCollectionMediaForm', [scollection_id]), '', function (t) {
             fcom.removeLoader();
@@ -595,11 +614,11 @@ $(document).on("change", ".state", function () {
     bannerPopupImage = function (inputBtn) {
         loadCropperSkeleton();
         $("#modalBoxJs .modal-title").text($(inputBtn).attr('data-name'));
-        if (inputBtn.files && inputBtn.files[0]) {            
+        if (inputBtn.files && inputBtn.files[0]) {
             fcom.updateWithAjax(fcom.makeUrl('Seller', 'imgCropper'), '', function (t) {
                 $("#modalBoxJs .modal-body").html(t.body);
                 $("#modalBoxJs .modal-footer").html(t.footer);
-                
+
                 var file = inputBtn.files[0];
                 var minWidth = document.frmShopBanner.banner_min_width.value;
                 var minHeight = document.frmShopBanner.banner_min_height.value;
@@ -616,7 +635,7 @@ $(document).on("change", ".state", function () {
                     imageSmoothingEnabled: true,
                 };
                 $(inputBtn).val('');
-               setTimeout(function () { cropImage(file, options, 'uploadShopImages', inputBtn); }, 100);
+                setTimeout(function () { cropImage(file, options, 'uploadShopImages', inputBtn); }, 100);
             });
         }
     };
@@ -624,7 +643,7 @@ $(document).on("change", ".state", function () {
     logoPopupImage = function (inputBtn) {
         loadCropperSkeleton();
         $("#modalBoxJs .modal-title").text($(inputBtn).attr('data-name'));
-        if (inputBtn.files && inputBtn.files[0]) {          
+        if (inputBtn.files && inputBtn.files[0]) {
             fcom.updateWithAjax(fcom.makeUrl('Seller', 'imgCropper'), '', function (t) {
                 $("#modalBoxJs .modal-body").html(t.body);
                 $("#modalBoxJs .modal-footer").html(t.footer);
@@ -655,18 +674,18 @@ $(document).on("change", ".state", function () {
     };
 
     uploadShopImages = function (formData) {
-        var frmName = formData.get("frmName");      
+        var frmName = formData.get("frmName");
         if ('frmShopLogo' == frmName) {
             var langId = document.frmShopLogo.lang_id.value;
             var fileType = document.frmShopLogo.file_type.value;
             var imageType = 'logo';
-            var ratio_type = $('input[name="ratio_type"]:checked').val();           
+            var ratio_type = $('input[name="ratio_type"]:checked').val();
         } else {
             var langId = document.frmShopBanner.lang_id.value;
             var slideScreen = document.frmShopBanner.slide_screen.value;
             var fileType = document.frmShopBanner.file_type.value;
             var imageType = 'banner';
-            var ratio_type = 0;            
+            var ratio_type = 0;
         }
 
         formData.append('slide_screen', slideScreen);
@@ -682,16 +701,16 @@ $(document).on("change", ".state", function () {
             data: formData,
             cache: false,
             contentType: false,
-            processData: false,           
+            processData: false,
             success: function (ans) {
                 $("#modalBoxJs").modal("hide");
-                $.mbsmessage.close();                      
+                $.mbsmessage.close();
                 if (ans.status == true) {
                     $.mbsmessage(ans.msg, true, 'alert--success');
                     shopImages(imageType, slideScreen, langId);
                 } else {
                     $.mbsmessage(ans.msg, true, 'alert--danger');
-                } 
+                }
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
@@ -759,7 +778,7 @@ $(document).on("change", ".state", function () {
                     $(dv).removeClass('badge-success');
                     $(dv).addClass('text-danger');
                 }
-                
+
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
