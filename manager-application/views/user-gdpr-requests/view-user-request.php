c2bbf511544737d2e8ee5fc622018205a5ca2281
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-03-03 18:07:37 +0530

    bug-062899 - Admin UI: Getting a UI design issue on the following modules

--- manager-application/views/user-gdpr-requests/view-user-request.php
+++ manager-application/views/user-gdpr-requests/view-user-request.php
@@ -8,40 +8,30 @@
     <div class="modal-body form-edit pd-0">
         <div class="form-edit-body loaderContainerJs">
             <form class="modal-body form pd-0">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label class="label"><?php echo Labels::getLabel('LBL_Full_Name', $siteLangId); ?></label>
-                            <div class=""><?php echo CommonHelper::displayNotApplicable($siteLangId, $userRequest['user_name']); ?></div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label class="label"><?php echo Labels::getLabel('LBL_Email', $siteLangId); ?></label>
-                            <div class=""><?php echo $userRequest['credential_email']; ?></div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label class="label"><?php echo Labels::getLabel('LBL_Username', $siteLangId); ?></label>
-                            <div class=""><?php echo $userRequest['credential_username']; ?></div>
-                        </div>
-                    </div>
+                <ul class="list-stats list-stats-double">
+                    <li class="list-stats-item">
+                        <span class="lable"><?php echo Labels::getLabel('LBL_Full_Name', $siteLangId); ?></span>
+                        <span class="value"><?php echo CommonHelper::displayNotApplicable($siteLangId, $userRequest['user_name']); ?></span>
+                    </li>
+                    <li class="list-stats-item">
+                        <span class="lable"><?php echo Labels::getLabel('LBL_Email', $siteLangId); ?></span>
+                        <span class="value"><?php echo $userRequest['credential_email']; ?></span>
+                    </li>
+                    <li class="list-stats-item">
+                        <span class="lable"><?php echo Labels::getLabel('LBL_Username', $siteLangId); ?></span>
+                        <span class="value"><?php echo $userRequest['credential_username']; ?></span>
+                    </li>
                     <?php if (!empty($userRequest['ureq_purpose'])) { ?>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label class="label"><?php echo Labels::getLabel('LBL_Purpose_of_request', $siteLangId); ?></label>
-                                <div class=""><?php echo nl2br($userRequest['ureq_purpose']); ?></div>
-                            </div>
-                        </div>
+                        <li class="list-stats-item">
+                            <span class="lable"><?php echo Labels::getLabel('LBL_Purpose_of_request', $siteLangId); ?></span>
+                            <span class="value"><?php echo nl2br($userRequest['ureq_purpose']); ?></span>
+                        </li>
                     <?php } ?>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label class="label"><?php echo Labels::getLabel('LBL_Request_Date', $siteLangId); ?></label>
-                            <div class=""><?php echo FatDate::format($userRequest['ureq_date'],true); ?></div>
-                        </div>
-                    </div>
-                </div>
+                    <li class="list-stats-item">
+                        <span class="lable"><?php echo Labels::getLabel('LBL_Request_Date', $siteLangId); ?></span>
+                        <span class="value"><?php echo FatDate::format($userRequest['ureq_date'], true); ?></span>
+                    </li>
+                </ul>
             </form>
         </div>
     </div>
--- readme.txt
+++ readme.txt
@@ -528,6 +528,7 @@ Updates/Fixes :
     => 062873 - Admin UI: Pagination is not working on the admin user's permission page
     => 062864 - Admin UI: Admin is not able to edit the records under the "Categories request" section
     => 062862 - Admin UI: The buyer's image is not showing properly under the messages' section on the 'order return request' page
+    => 062899 - Admin UI: Getting a UI design issue on the following modules
     
 Known Issues and Problems :
     => 82248 : Renaming existing DPO Payment Gateway to Paygate as it belongs to South Africa linked with Dpo Group.
