b6836bc14c90acf9b1b3dcf95240f5bd282ae210
ablyanup <anup.rawat@ablysoft.com>
2022-03-10 14:22:19 +0530

    task-90877 admin dashboard stats optimization and enhancement

--- manager-application/views/home/total-sales.php
+++ manager-application/views/home/total-sales.php
@@ -1,5 +1,12 @@
 <ul class="list-stats list-stats-inline">
-    <?php $cls = ($orderSalesStats["totalsales"] > 0) ? 'fa-arrow-up font-success' : 'fa-arrow-down font-error'; ?>
+    <?php
+    $cls = '';
+    if ($orderSalesStats["totalsales"] > 0) {
+        $cls = 'fa-arrow-up font-success';
+    } else if ($orderSalesStats["totalsales"] < 0) {
+        $cls = 'fa-arrow-down font-error';
+    }
+    ?>
     <li class="list-stats-item">
         <span class="label">
             <i class="dot" style="background-color:#f05b4f;"></i>
@@ -9,7 +16,14 @@
             <?php echo CommonHelper::displayMoneyFormat($orderSalesStats["totalsales"], true, true); ?></span>
     </li>
     <li class="list-stats-item">
-        <?php $cls = ($orderSalesStats["totalcommission"] > 0) ? 'fa-arrow-up font-success' : 'fa-arrow-down font-error'; ?>
+        <?php
+        $cls = '';
+        if ($orderSalesStats["totalcommission"] > 0) {
+            $cls = 'fa-arrow-up font-success';
+        } else if ($orderSalesStats["totalcommission"] < 0) {
+            $cls = 'fa-arrow-down font-error';
+        }
+        ?>
         <span class="label">
             <i class="dot" style="background-color: #f4c63d;"></i>
             <?php echo Labels::getLabel('LBL_SALES_EARNINGS', $siteLangId); ?></span>
@@ -19,14 +33,27 @@
         </span>
     </li>
     <li class="list-stats-item">
-        <?php $cls = ($userSignupStats > 0) ? 'fa-arrow-up font-success' : 'fa-arrow-down font-error'; ?>
+        <?php $cls = '';
+        if ($userSignupStats > 0) {
+            $cls = 'fa-arrow-up font-success';
+        } else if ($userSignupStats < 0) {
+            $cls = 'fa-arrow-down font-error';
+        }
+        ?>
+
         <span class="label"> <i class="dot" style="background-color:#008000;"></i><?php echo Labels::getLabel('LBL_NEW_USERS', $siteLangId); ?></span>
         <span class="value">
             <i class="icn fas <?php echo $cls; ?>"></i>
             <?php echo $userSignupStats; ?></span>
     </li>
     <li class="list-stats-item">
-        <?php $cls = ($shopsSignupStats['shopSignups'] > 0) ? 'fa-arrow-up font-success' : 'fa-arrow-down font-error'; ?>
+        <?php $cls = '';
+        if ($shopsSignupStats['shopSignups'] > 0) {
+            $cls = 'fa-arrow-up font-success';
+        } else if ($shopsSignupStats['shopSignups'] < 0) {
+            $cls = 'fa-arrow-down font-error';
+        }
+        ?>
         <span class="label"> <i class="dot" style="background-color:#d17905;"></i>
             <?php echo Labels::getLabel('LBL_NEW_SHOPS', $siteLangId); ?></span>
         <span class="value">
