8f2c492d1e864f4a90088b93bef48c8d5a98d424
ablyshashi <shashi.kumar@fatbit.in>
2022-03-11 14:57:30 +0530

    bug-061818 - Admin UI: Category's media is not clickable, brands' uploaded media is not showing and getting an 'invalid request' error while removing the rating types while adding the category & brands on the product's page

--- manager-application/views/product-categories/form.php
+++ manager-application/views/product-categories/form.php
@@ -82,8 +82,7 @@ require_once(CONF_THEME_PATH . '_partial/listing/form.php'); ?>
 
         removeRatingType = function(e) {
             var rt_id = e.detail.tag.id;      
-            var prodCatId = $("input[name='prodcat_id']").val();
-            console.log(prodCatId);
+            var prodCatId = $("input[name='prodcat_id']").val();           
             if ('' == rt_id || 1 > prodCatId) {
                 return;
             }
