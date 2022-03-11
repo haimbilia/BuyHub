aee0cd8c0a9015458e35ed04e0dee40c036bd298
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2022-03-11 12:49:05 +0530

    task-95714 continued working on Shop page UI

--- application/views/shops/shop-layout-navigation.php
+++ application/views/shops/shop-layout-navigation.php
@@ -1,26 +1,29 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage'); ?>
 <ul class="shop-nav">
-    <li class="shop-nav-item <?php echo $action == 'view' ? 'active' : '' ?>"><a class="shop-nav-link" href="<?php echo UrlHelper::generateUrl('shops', 'view', array($shop_id)); ?>"><?php echo Labels::getLabel('LBL_SHOP_STORE_HOME', $siteLangId); ?>
-        </a></li>
+    <li class="shop-nav-item">
+        <a class="shop-nav-link <?php echo $action == 'view' ? 'active' : '' ?>" href="<?php echo UrlHelper::generateUrl('shops', 'view', array($shop_id)); ?>">
+            <?php echo Labels::getLabel('LBL_SHOP_STORE_HOME', $siteLangId); ?>
+        </a>
+    </li>
     <?php /* if (!empty($collectionData)) { ?>
     <li class="<?php echo $action == 'collection' ? 'active' : '' ?>"><a href="<?php echo UrlHelper::generateUrl('shops', 'collections', array($shop_id));?>" ><?php echo $collectionData['collectionName']; ?></a></li>
     <?php } */ ?>
     <?php if (0 < FatApp::getConfig("CONF_ALLOW_REVIEWS", FatUtility::VAR_INT, 0)) { ?>
-        <li class="shop-nav-item <?php echo $action == 'topProducts' ? 'active' : '' ?>">
-            <a class="shop-nav-link" href="<?php echo UrlHelper::generateUrl('shops', 'topProducts', array($shop_id)); ?>">
+        <li class="shop-nav-item">
+            <a class="shop-nav-link <?php echo $action == 'topProducts' ? 'active' : '' ?>" href="<?php echo UrlHelper::generateUrl('shops', 'topProducts', array($shop_id)); ?>">
                 <?php echo Labels::getLabel('LBL_SHOP_TOP_PRODUCTS', $siteLangId); ?></a>
         </li>
-        <li class="shop-nav-item  <?php echo $action == 'shop' ? 'active' : '' ?>">
-            <a class="shop-nav-link " href="<?php echo UrlHelper::generateUrl('reviews', 'shop', array($shop_id)); ?>"><?php echo Labels::getLabel('LBL_SHOP_REVIEW', $siteLangId); ?></a>
+        <li class="shop-nav-item">
+            <a class="shop-nav-link <?php echo $action == 'shop' ? 'active' : '' ?>" href="<?php echo UrlHelper::generateUrl('reviews', 'shop', array($shop_id)); ?>"><?php echo Labels::getLabel('LBL_SHOP_REVIEW', $siteLangId); ?></a>
         </li>
     <?php } ?>
     <?php if (!UserAuthentication::isUserLogged() || (UserAuthentication::isUserLogged() && ((User::isBuyer()) || (User::isSeller())) && (UserAuthentication::getLoggedUserId() != $shop_user_id))) { ?>
-        <li class="shop-nav-item <?php echo $action == 'sendMessage' ? 'active' : '' ?>">
-            <a class="shop-nav-link " href="<?php echo UrlHelper::generateUrl('shops', 'sendMessage', array($shop_id)); ?>"><?php echo Labels::getLabel('LBL_SHOP_CONTACT', $siteLangId); ?></a>
+        <li class="shop-nav-item">
+            <a class="shop-nav-link <?php echo $action == 'sendMessage' ? 'active' : '' ?>" href="<?php echo UrlHelper::generateUrl('shops', 'sendMessage', array($shop_id)); ?>"><?php echo Labels::getLabel('LBL_SHOP_CONTACT', $siteLangId); ?></a>
         </li>
     <?php } ?>
-    <li class="shop-nav-item <?php echo $action == 'policy' ? 'active' : '' ?>">
-        <a class="shop-nav-link " href="<?php echo UrlHelper::generateUrl('shops', 'policy', array($shop_id)); ?>">
+    <li class="shop-nav-item">
+        <a class="shop-nav-link <?php echo $action == 'policy' ? 'active' : '' ?>" href="<?php echo UrlHelper::generateUrl('shops', 'policy', array($shop_id)); ?>">
             <?php echo Labels::getLabel('LBL_SHOP_DETAILS', $siteLangId); ?>
         </a>
     </li>
--- manager-application/views/_partial/ribbon-ui.php
+++ manager-application/views/_partial/ribbon-ui.php
@@ -9,19 +9,19 @@ if (is_array($ribbRow) && !empty($ribbRow)) {
         $text = "";
     }
     $class = '';
-    if (array_key_exists('blinkcond_position', $ribbRow)) {        
-        $class = ( Badge::RIBB_POS_TLEFT == $ribbRow['blinkcond_position'])  ? 'badges-left' : 'badges-right';
+    if (array_key_exists('blinkcond_position', $ribbRow)) {
+        $class = (Badge::RIBB_POS_TLEFT == $ribbRow['blinkcond_position'])  ? 'badges-left' : 'badges-right';
     }
 
     switch ($type) {
         case Badge::SHAPE_RECTANGLE:
-            $ribbon = '<div class="badges badges-' . $type . ' ' . $class . '" style="background:' . $color . '" title="' . $title . '">' . $text . '</div>';
+            $ribbon = '<div class="badge badges-' . $type . ' ' . $class . '" style="background:' . $color . '" title="' . $title . '">' . $text . '</div>';
             break;
         case Badge::SHAPE_STRIP:
         case Badge::SHAPE_STAR:
         case Badge::SHAPE_TRIANGLE:
         case Badge::SHAPE_CIRCLE:
-            $ribbon = '<div class="badges badges-' . $type . ' ' . $class . '" title="' . $title . '">
+            $ribbon = '<div class="badge badges-' . $type . ' ' . $class . '" title="' . $title . '">
                         <svg class="svg" style="fill:' . $color . '">
                             <use xlink:href="' . CONF_WEBROOT_FRONT_URL . 'images/retina/badges/sprite.svg#badges-' . $type . '"></use>
                         </svg>
