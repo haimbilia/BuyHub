ea90955b9527f6f4825912e8e2cc11fe0e3eccf5
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-03-16 14:57:38 +0530

    task-94791-Offcanvas part footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -206,21 +206,7 @@ if (CommonHelper::demoUrl()) { ?>
     $menu.find(".mm-next").addClass("mm-fullsubopen");
 </script>
 
-<!-- offcanvas-mega-search -->
-<div class="offcanvas offcanvas-mega-search" data-bs-backdrop="false" tabindex="-1" id="mega-nav-search" aria-labelledby="mega-nav-searchLabel">
-    <?php $this->includeTemplate('_partial/headerSearchFormArea.php'); ?>
-</div>
-
-<!-- offcanvas-filters -->
-<div class="offcanvas offcanvas-end  offcanvas-filters" tabindex="-1" id="filters-right" aria-labelledby="filters-right">
-    <div class="offcanvas-header">
-        <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-        Filter Right
-    </div>
-</div>
+<?php include(CONF_THEME_PATH . '_partial/footer-part/offcanvas-elements.php'); ?>
 
 </body>
 
