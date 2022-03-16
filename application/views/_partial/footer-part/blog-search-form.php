b40a8dbef3bdab0e11f81b75334453b491433349
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-03-16 16:00:35 +0530

    task-94791-Offcanvas popup part moved to footer section

---
+++ application/views/_partial/footer-part/blog-search-form.php
@@ -0,0 +1,20 @@
+<?php defined('SYSTEM_INIT') or die('Invalid Usage'); ?>
+<!-- offcanvas-blog-search -->
+<div class="offcanvas offcanvas-blog-search" data-bs-backdrop="false" tabindex="-1" id="blog-search" aria-labelledby="blog-searchLabel">
+    <div class="blog-search">
+        <?php $blogSearchFrm->setFormTagAttribute('onSubmit', 'submitBlogSearch(this); return(false);');
+        $blogSearchFrm->setFormTagAttribute('class', 'form');
+        $blogSearchFrm->developerTags['colClassPrefix'] = 'col-lg-12 col-md-12 col-sm-';
+        $blogSearchFrm->developerTags['fld_default_col'] = 12;
+        $keywordFld = $blogSearchFrm->getField('keyword');
+        $keywordFld->setFieldTagAttribute('class', 'blog-search-input');
+        $keywordFld->setFieldTagAttribute('placeholder', Labels::getLabel('LBL_Search_In_Blogs...'));
+        $submitFld = $blogSearchFrm->getField('btnProductSrchSubmit');
+        $submitFld->setFieldTagAttribute('class', 'btn');
+        echo $blogSearchFrm->getFormTag();
+        echo $blogSearchFrm->getFieldHTML('keyword');
+        echo $blogSearchFrm->getFieldHTML('btnProductSrchSubmit');
+        echo $blogSearchFrm->getExternalJS(); ?>
+        </form>
+    </div>
+</div>
--- application/views/_partial/footer-part/offcanvas-elements.php
+++ application/views/_partial/footer-part/offcanvas-elements.php
@@ -1,8 +1,12 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-<!-- offcanvas-mega-search -->
-<div class="offcanvas offcanvas-mega-search" data-bs-backdrop="false" tabindex="-1" id="mega-nav-search" aria-labelledby="mega-nav-searchLabel">
-    <?php $this->includeTemplate('_partial/headerSearchFormArea.php'); ?>
-</div>
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
+
+if ($controllerName != 'Cart') {
+    /* offcanvas-side-cart */
+    $this->includeTemplate('_partial/footer-part/cart-summary.php');
+} ?>
+
+<!-- Header Search Form -->
+<?php $this->includeTemplate('_partial/footer-part/headerSearchFormArea.php'); ?>
 
 <!-- offcanvas-filters -->
 <div class="offcanvas offcanvas-end  offcanvas-filters" tabindex="-1" id="filters-right" aria-labelledby="filters-right">
@@ -14,3 +18,150 @@
         Filter Right
     </div>
 </div>
+
+<!-- offcanvas-account -->
+<div class="offcanvas offcanvas-account offcanvas-start" tabindex="-1" id="offcanvas-account">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title"> </h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+
+        <div class="profile">
+            <div class="profile-image">
+                <img class="profile-avatar" src="<?php echo CONF_WEBROOT_URL; ?>images/users/100_2.jpg" alt="">
+            </div>
+            <div class="profile-data">
+                <h6 class="profile-name">Hi, Michael Williams </h6>
+            </div>
+            <div class="geo-location">
+                <div class="geo-location_inner">
+                    <?php if (FatApp::getConfig('CONF_ENABLE_GEO_LOCATION', FatUtility::VAR_INT, 0) && !empty(FatApp::getConfig('CONF_GOOGLEMAP_API_KEY', FatUtility::VAR_STRING, ''))) { ?>
+                        <div class="dropdown">
+                            <?php
+                            $geoAddress = '';
+                            if ((!isset($_COOKIE['_ykGeoLat']) || !isset($_COOKIE['_ykGeoLng']) || !isset($_COOKIE['_ykGeoCountryCode'])) && FatApp::getConfig('CONF_DEFAULT_GEO_LOCATION', FatUtility::VAR_INT, 0)) {
+                                $geoAddress = FatApp::getConfig('CONF_GEO_DEFAULT_ADDR', FatUtility::VAR_STRING, '');
+                                if (empty($address)) {
+                                    $address = FatApp::getConfig('CONF_GEO_DEFAULT_ZIPCODE', FatUtility::VAR_INT, 0) . '-' . FatApp::getConfig('CONF_GEO_DEFAULT_STATE', FatUtility::VAR_STRING, '');
+                                }
+                            }
+                            if (empty($geoAddress)) {
+                                $geoAddress = Labels::getLabel("LBL_Location", $siteLangId);
+                            }
+                            $geoAddress =  isset($_COOKIE["_ykGeoAddress"]) ? $_COOKIE["_ykGeoAddress"] : $geoAddress;
+                            ?>
+                            <button class="button-geo-location geo-location_trigger" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="icn">
+                                    <svg class="svg" width="18" height="18">
+                                        <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#location">
+                                        </use>
+                                    </svg>
+                                </i>
+                                <div class="geo-location-selected">
+                                    <?php echo $geoAddress; ?>
+                                </div>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-fit dropdown-menu-anim geo-location_dropdown-menu" aria-labelledby="location-dropdown">
+                                <div class="geo-location_body">
+
+                                    <input autocomplete="no" id="ga-autoComplete-header" class="geo-location_input pac-target-input" title="<?php echo Labels::getLabel('LBL_TYPE_YOUR_ADDRESS', $siteLangId); ?>" placeholder="<?php echo Labels::getLabel('LBL_TYPE_YOUR_ADDRESS', $siteLangId); ?>" type="text" name="location" value="<?php echo $geoAddress; ?>">
+                                    <!-- <div class="or">
+                                            <span>Or</span>
+                                        </div> -->
+                                    <button onclick="loadGeoLocation()" class="btn btn-outline-gray btn-block btn-detect">
+                                        <i class="icn">
+                                            <svg class="svg" width="18" height="18">
+                                                <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#gps">
+                                                </use>
+                                            </svg>
+                                        </i>
+                                        <span class="txt">
+                                            <?php echo Labels::getLabel('LBL_DETECT_MY_CURRENT_LOCATION', $siteLangId); ?>
+                                        </span>
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="offcanvas-foot">
+        <button class="btn btn-logout" type="button">
+            <i class="icn">
+                <svg class="svg" width="20" height="20">
+                    <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#logout">
+                    </use>
+                </svg>
+            </i>
+            logout</button>
+    </div>
+</div>
+
+<!-- offcanvas-gps-location -->
+<div class="offcanvas offcanvas-gps-location offcanvas-bottom" tabindex="-1" id="offcanvas-gps-location">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title"> </h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div class="geo-location">
+            <div class="geo-location_inner">
+                <?php if (FatApp::getConfig('CONF_ENABLE_GEO_LOCATION', FatUtility::VAR_INT, 0) && !empty(FatApp::getConfig('CONF_GOOGLEMAP_API_KEY', FatUtility::VAR_STRING, ''))) { ?>
+                    <div class="dropdown">
+                        <?php
+                        $geoAddress = '';
+                        if ((!isset($_COOKIE['_ykGeoLat']) || !isset($_COOKIE['_ykGeoLng']) || !isset($_COOKIE['_ykGeoCountryCode'])) && FatApp::getConfig('CONF_DEFAULT_GEO_LOCATION', FatUtility::VAR_INT, 0)) {
+                            $geoAddress = FatApp::getConfig('CONF_GEO_DEFAULT_ADDR', FatUtility::VAR_STRING, '');
+                            if (empty($address)) {
+                                $address = FatApp::getConfig('CONF_GEO_DEFAULT_ZIPCODE', FatUtility::VAR_INT, 0) . '-' . FatApp::getConfig('CONF_GEO_DEFAULT_STATE', FatUtility::VAR_STRING, '');
+                            }
+                        }
+                        if (empty($geoAddress)) {
+                            $geoAddress = Labels::getLabel("LBL_Location", $siteLangId);
+                        }
+                        $geoAddress =  isset($_COOKIE["_ykGeoAddress"]) ? $_COOKIE["_ykGeoAddress"] : $geoAddress;
+                        ?>
+                        <button class="button-geo-location geo-location_trigger" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                            <svg class="svg" width="18" height="18">
+                                <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#location">
+                                </use>
+                            </svg>
+
+                            <div class="geo-location-selected">
+                                <?php echo $geoAddress; ?>
+                            </div>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-fit dropdown-menu-anim geo-location_dropdown-menu" aria-labelledby="location-dropdown">
+                            <div class="geo-location_body"> <input autocomplete="no" id="ga-autoComplete-header" class="geo-location_input pac-target-input" title="<?php echo Labels::getLabel('LBL_TYPE_YOUR_ADDRESS', $siteLangId); ?>" placeholder="<?php echo Labels::getLabel('LBL_TYPE_YOUR_ADDRESS', $siteLangId); ?>" type="text" name="location" value="<?php echo $geoAddress; ?>">
+                                <!-- <div class="or">
+                                        <span>Or</span>
+                                    </div> -->
+                                <button onclick="loadGeoLocation()" class="btn btn-brand btn-block btn-detect">
+
+                                    <svg class="svg" width="18" height="18">
+                                        <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#gps">
+                                        </use>
+                                    </svg>
+
+                                    <span class="txt">
+                                        <?php echo Labels::getLabel('LBL_DETECT_MY_CURRENT_LOCATION', $siteLangId); ?>
+                                    </span>
+                                </button>
+
+
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Blog Search Form -->
+<?php $this->includeTemplate('_partial/footer-part/blog-search-form.php'); ?>
