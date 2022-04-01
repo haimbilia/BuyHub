0798cc3d4a4a3c2329babec7841993a015225baa
ablyanup <anup.rawat@ablysoft.com>
2022-04-01 16:01:44 +0530

    task-94333 home page optimization

--- application/mobile-views/header.php
+++ application/mobile-views/header.php
@@ -1,9 +1,9 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 $status = applicationConstants::ON;
 $commonData = array(
-    'currencySymbol'=>$currencySymbol,
-    'totalFavouriteItems'=>$totalFavouriteItems,
-    'totalUnreadMessageCount'=>$totalUnreadMessageCount,
-    'totalUnreadNotificationCount'=>$totalUnreadNotificationCount,
-    'cartItemsCount'=>$cartItemsCount
+    'currencySymbol' => $currencySymbol,
+    'totalFavouriteItems' => $totalFavouriteItems ?? 0,
+    'totalUnreadMessageCount' => $totalUnreadMessageCount ?? 0,
+    'totalUnreadNotificationCount' => $totalUnreadNotificationCount ?? 0,
+    'cartItemsCount' => $cartItemsCount
 );
