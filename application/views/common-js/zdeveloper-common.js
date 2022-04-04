37335820491aedc6ff728d458508ba2819f3a0c1
ablyshashi <shashi.kumar@fatbit.in>
2022-04-04 10:47:22 +0530

    task-94791-user redirect issue and unwanted js removed

--- application/views/common-js/zdeveloper-common.js
+++ application/views/common-js/zdeveloper-common.js
@@ -1322,8 +1322,8 @@ $(function () {
                 if (ans.status == 0) {
                     fcom.displayErrorMessage(ans.msg);
                 } else {
-                    $(".cookie-alert").hide("slow");
-                    $(".cookie-alert").remove();
+                    $("#cookieInfoBox").hide("slow");
+                    $("#cookieInfoBox").remove();
                     $.facebox.close();
                 }
             }
