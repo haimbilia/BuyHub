81e897f1ce8ee9404a539728aea357eae0c5c106
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-04-13 09:19:48 +0530

    task-94791-Form event onclear move form to sidebar

--- manager-application/views/order-cancellation-requests/form.php
+++ manager-application/views/order-cancellation-requests/form.php
@@ -1,10 +1,11 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 $frm->setFormTagAttribute('onsubmit', 'saveRecord($("#' . $frm->getFormTagAttribute('id') . '")[0], "closeForm"); return(false);');
-$frm->getField('ocrequest_status')->setFieldTagAttribute('id','ocrequest_status');
+$frm->setFormTagAttribute('data-onclear', 'editRecord(' . $recordId . ',true)');
+$frm->getField('ocrequest_status')->setFieldTagAttribute('id', 'ocrequest_status');
 
 $fld = $frm->getField('ocrequest_refund_in_wallet');
-$fld->setFieldTagAttribute('id','ocrequest_refund_in_wallet');
-$fld->setFieldTagAttribute('disabled',true);
+$fld->setFieldTagAttribute('id', 'ocrequest_refund_in_wallet');
+$fld->setFieldTagAttribute('disabled', true);
 
 $fld = $frm->getField('ocrequest_refund_in_wallet');
 $fld->developerTags['rdLabelAttributes'] = ['class' => 'radio'];
