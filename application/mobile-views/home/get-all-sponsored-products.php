ac6954b7b58e17f22fc4097244f7c46a58970254
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-04-22 09:21:26 +0530

    task-70452-Various API response updated with new theprice value

--- application/mobile-views/home/get-all-sponsored-products.php
+++ application/mobile-views/home/get-all-sponsored-products.php
@@ -6,7 +6,7 @@ foreach ($sponsoredProds as $index => &$product) {
     $product['product_image_url'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], ImageDimension::VIEW_CLAYOUT3, $product['selprod_id'], 0, $siteLangId)).$uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
     $product['discount'] = ($product['special_price_found'] && $product['selprod_price'] > $product['theprice']) ? CommonHelper::showProductDiscountedText($product, $siteLangId) : '';
     $product['selprod_price'] = CommonHelper::displayMoneyFormat($product['selprod_price'], true, true, true);
-    $product['theprice'] = CommonHelper::displayMoneyFormat($product['theprice'], true, true, true);
+    $product['theprice'] = CommonHelper::displayMoneyFormat($product['theprice']);
 }
 
 $data = array(
--- application/mobile-views/products/index.php
+++ application/mobile-views/products/index.php
@@ -6,6 +6,6 @@ if (array_key_exists('products', $data)) {
         $product['product_image_url'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('image', 'product', array($product['product_id'], ImageDimension::VIEW_CLAYOUT3, $product['selprod_id'], 0, $siteLangId)).$uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
         $product['discount'] = ($product['special_price_found'] && $product['selprod_price'] > $product['theprice']) ? CommonHelper::showProductDiscountedText($product, $siteLangId) : '';
         $product['selprod_price'] = CommonHelper::displayMoneyFormat($product['selprod_price'], true, true, true);
-        $product['theprice'] = CommonHelper::displayMoneyFormat($product['theprice'], true, true, true);
+        $product['theprice'] = CommonHelper::displayMoneyFormat($product['theprice']);
     }
 }
