b745e6a3a8c99d0bde2ff49c20467ad17f1b2340
ablyanup <anup.rawat@ablysoft.com>
2022-04-29 11:44:24 +0530

    task-94333 optimization in prehooks

--- dashboard/utilities/prehook.php
+++ dashboard/utilities/prehook.php
@@ -7,7 +7,8 @@ define('CONF_FORM_REQUIRED_STAR_POSITION', Form::FORM_REQUIRED_STAR_POSITION_AFT
 FatApplication::getInstance()->setControllersForStaticFileServer(array('images', 'img', 'fonts', 'templates', 'innovas', 'assetmanager'));
 
 $innova_settings = array(
-    /* 'width' => '730', */ 'height' => '400', 'arrStyle' => '[["body",false,"","min-height:250px;"]]',  'groups' => ' [
+    /* 'width' => '730', */
+    'height' => '400', 'arrStyle' => '[["body",false,"","min-height:250px;"]]',  'groups' => ' [
     ["group1", "", ["Bold", "Italic", "Underline", "FontName", "ForeColor", "TextDialog", "RemoveFormat"]],
     ["group2", "", ["Bullets", "Numbering", "JustifyLeft", "JustifyCenter", "JustifyRight"]],
     ["group3", "", ["LinkDialog", "ImageDialog", "YoutubeDialog", "Table", "TableDialog"]],
@@ -15,15 +16,17 @@ $innova_settings = array(
     'fileBrowser' => '"' . CONF_WEBROOT_URL . 'innova/assetmanager/asset.php"'
 );
 
-FatApp::setViewDataProvider('_partial/buyerDashboardNavigation.php', array('Navigation', 'buyerDashboardNavigation'));
-FatApp::setViewDataProvider('_partial/advertiser/advertiserDashboardNavigation.php', array('Navigation', 'advertiserDashboardNavigation'));
-FatApp::setViewDataProvider('_partial/seller/sellerDashboardNavigation.php', array('Navigation', 'sellerDashboardNavigation'));
-FatApp::setViewDataProvider('_partial/affiliate/affiliateDashboardNavigation.php', array('Navigation', 'affiliateDashboardNavigation'));
-FatApp::setViewDataProvider('_partial/topHeaderDashboard.php', array('Navigation', 'topHeaderDashboard'));
+if (!FatUtility::isAjaxCall()) {
+    FatApp::setViewDataProvider('_partial/buyerDashboardNavigation.php', array('Navigation', 'buyerDashboardNavigation'));
+    FatApp::setViewDataProvider('_partial/advertiser/advertiserDashboardNavigation.php', array('Navigation', 'advertiserDashboardNavigation'));
+    FatApp::setViewDataProvider('_partial/seller/sellerDashboardNavigation.php', array('Navigation', 'sellerDashboardNavigation'));
+    FatApp::setViewDataProvider('_partial/affiliate/affiliateDashboardNavigation.php', array('Navigation', 'affiliateDashboardNavigation'));
+    FatApp::setViewDataProvider('_partial/topHeaderDashboard.php', array('Navigation', 'topHeaderDashboard'));
 
-FatApp::setViewDataProvider('_partial/headerUserArea.php', array('Common', 'headerUserArea'));
-FatApp::setViewDataProvider('_partial/dashboardLanguageArea.php', array('Common', 'headerLanguageArea'));
-FatApp::setViewDataProvider('_partial/header/header-breadcrumb.php', array('Common', 'setHeaderBreadCrumb'));
-// FatApp::setViewDataProvider('_partial/userDashboardMessages.php', array('Common', 'userMessages'));
-FatApp::setViewDataProvider('_partial/footerNavigation.php', array('Navigation', 'footerNavigation'));
-FatApp::setViewDataProvider('_partial/seller/sellerSalesGraph.php', array('Statistics', 'sellerSalesGraph'));
+    FatApp::setViewDataProvider('_partial/headerUserArea.php', array('Common', 'headerUserArea'));
+    FatApp::setViewDataProvider('_partial/dashboardLanguageArea.php', array('Common', 'headerLanguageArea'));
+    FatApp::setViewDataProvider('_partial/header/header-breadcrumb.php', array('Common', 'setHeaderBreadCrumb'));
+    // FatApp::setViewDataProvider('_partial/userDashboardMessages.php', array('Common', 'userMessages'));
+    FatApp::setViewDataProvider('_partial/footerNavigation.php', array('Navigation', 'footerNavigation'));
+    FatApp::setViewDataProvider('_partial/seller/sellerSalesGraph.php', array('Statistics', 'sellerSalesGraph'));
+}
