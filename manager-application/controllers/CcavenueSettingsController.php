1746ab52aa0f60651eece7096c9a966831f848e1
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-05-31 14:28:40 +0530

    task-98247-Change label in payment gateway settings

--- manager-application/controllers/CcavenueSettingsController.php
+++ manager-application/controllers/CcavenueSettingsController.php
@@ -10,7 +10,7 @@ class CcavenueSettingsController extends PaymentMethodSettingsController
         $envFld->requirement->setRequired(true);
         $frm->addRequiredField(Labels::getLabel('FRM_MERCHANT_ID', $langId), 'merchant_id');
         $frm->addRequiredField(Labels::getLabel('FRM_ACCESS_CODE', $langId), 'access_code');
-        $frm->addTextBox(Labels::getLabel('FRM_ENCRYPTION_KEY', $langId), 'working_key');
+        $frm->addTextBox(Labels::getLabel('FRM_ENCRYPTION_KEY/_WORKING_KEY', $langId), 'working_key');
         $frm->addHTML('', '', '<p class="form-text">' . Labels::getLabel('LBL_CCAVENUE_TEST_CREDENTIALS') . '<p>');
         return $frm;
     }
