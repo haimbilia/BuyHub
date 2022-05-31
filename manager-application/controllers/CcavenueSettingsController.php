42b993643d82f3c2cba329ea941ba74a23c45b8e
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-05-31 14:18:41 +0530

    task-95487-CCAvenue payment gateway code enhancements

--- manager-application/controllers/CcavenueSettingsController.php
+++ manager-application/controllers/CcavenueSettingsController.php
@@ -5,6 +5,9 @@ class CcavenueSettingsController extends PaymentMethodSettingsController
     public static function form($langId)
     {
         $frm = new Form('frmPaymentMethods');
+        $envoirment = Plugin::getEnvArr($langId);
+        $envFld = $frm->addSelectBox(Labels::getLabel('FRM_ENVOIRMENT', $langId), 'env', $envoirment, '', [], '');
+        $envFld->requirement->setRequired(true);
         $frm->addRequiredField(Labels::getLabel('FRM_MERCHANT_ID', $langId), 'merchant_id');
         $frm->addRequiredField(Labels::getLabel('FRM_ACCESS_CODE', $langId), 'access_code');
         $frm->addTextBox(Labels::getLabel('FRM_ENCRYPTION_KEY', $langId), 'working_key');
