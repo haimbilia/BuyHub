d64e6a292051bf2348d53c36d533ce0015294514
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2022-05-31 17:45:19 +0530

    task-96131 created CSS for blog mobile canvas menu

--- application/views/_partial/footer-part/blog-mobile-menu.php
+++ application/views/_partial/footer-part/blog-mobile-menu.php
@@ -1,16 +1,23 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage'); ?>
 <!-- offcanvas-mobile-menu -->
 <div class="offcanvas offcanvas-start offcanvas-mobile-menu" tabindex="-1" id="blog-menu">
-    <?php if (!empty($categoriesArr)) { ?>
-        <ul class="nav-blog">
-            <?php
-            foreach ($categoriesArr as $id => $cat) { ?>
-                <li class="nav-blog-item">
-                    <a class="nav-blog-link" href="<?php echo UrlHelper::generateUrl('Blog', 'category', array($id)); ?>">
-                        <?php echo $cat; ?>
-                    </a>
-                </li>
-            <?php } ?>
-        </ul>
-    <?php } ?>
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title">Categories </h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body p-0">
+        <?php if (!empty($categoriesArr)) { ?>
+            <ul class="offcanvas-blog-nav">
+                <?php
+                foreach ($categoriesArr as $id => $cat) { ?>
+                    <li class="offcanvas-blog-nav-item">
+                        <a class="offcanvas-blog-nav-link" href="<?php echo UrlHelper::generateUrl('Blog', 'category', array($id)); ?>">
+                            <?php echo $cat; ?>
+                            <i class="icon icon-arrow-right"></i>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        <?php } ?>
+    </div>
 </div>
