f72aed9ec4800c1bdb8d7337a81ab43d9e318831
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-06-21 18:44:41 +0530

    bug-064797 - Suggestion to add the label for accepted extension type under the Digital files section.

--- manager-application/views/products/digital-download-form.php
+++ manager-application/views/products/digital-download-form.php
@@ -1,7 +1,7 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
 HtmlHelper::formatFormFields($frm);
-$frm->setFormTagAttribute('class', 'form');
+$frm->setFormTagAttribute('class', 'form modalFormJs');
 $frm->setFormTagAttribute('id', 'digitalDownloadFrm');
 $frm->setFormTagAttribute('enctype', 'multipart/form-data');
 
@@ -9,50 +9,56 @@ $frm->setFormTagAttribute('onsubmit', "setupDigitalDownload($('#digitalDownloadF
 $frm->setFormTagAttribute('data-onclear', "digitalDownloadsForm($type)");
 
 $fld = $frm->getField('option_comb_id');
-if($fld){
+if ($fld) {
     $fld->addFieldTagAttribute('id', "digitalFrmOptionId");
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
 $fld = $frm->getField('record_id');
-$fld->addFieldTagAttribute('id', "digitalFrmRecordId");   
+$fld->addFieldTagAttribute('id', "digitalFrmRecordId");
 $fld = $frm->getField('download_type');
 $fld->addFieldTagAttribute('id', "digitalFrmdownloadType");
 
 $fld = $frm->getField('attach_with_existing_orders');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
 $fld = $frm->getField('lang_id');
-if($fld){
+if ($fld) {
     $fld->addFieldTagAttribute('id', "digitalFrmLangId");
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
+$lbl = Labels::getLabel('LBL_ALLOWED_FILES_ENTENSIONS_{EXT}');
+$icon = '<i class="fas fa-exclamation-circle" data-bs-toggle="tooltip" title="' . implode(' | ', applicationConstants::allowedFileExtensions()) . '"></i>';
+$helpTxt = CommonHelper::replaceStringData($lbl, ['{EXT}' => $icon]);
+
 $fld = $frm->getField('downloadable_file');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
-    $fld->addWrapperAttribute('id','downloadableFileMainJs');
+    $fld->addWrapperAttribute('id', 'downloadableFileMainJs');
+    $fld->htmlAfterField = '<p class="form-text text-muted">' . $helpTxt . '</p>';
 }
 
 $fld = $frm->getField('preview_file');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
+    $fld->htmlAfterField = '<p class="form-text text-muted">' . $helpTxt . '</p>';
 }
 
 $fld = $frm->getField('product_downloadable_link');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
 $fld = $frm->getField('product_preview_link');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
 $fld = $frm->getField('product_preview_link');
-if($fld){
+if ($fld) {
     $fld->developerTags['colWidthValues'] = [null, '6', null, null];
 }
 
@@ -60,11 +66,10 @@ $includeTabs = false;
 HtmlHelper::formatFormFields($frm);
 
 require_once(CONF_THEME_PATH . '_partial/listing/form-head.php'); ?>
-    <div class="form-edit-body loaderContainerJs">
-        <?php echo $frm->getFormHtml(); ?>
-        <div id="digitalFrmListJs"></div>
-    </div>
-   <?php require_once(CONF_THEME_PATH . '_partial/listing/form-edit-foot.php');?>
+<div class="form-edit-body loaderContainerJs">
+    <?php echo $frm->getFormHtml(); ?>
+    <div id="digitalFrmListJs"></div>
+</div>
+<?php require_once(CONF_THEME_PATH . '_partial/listing/form-edit-foot.php'); ?>
 
 </div>
-
