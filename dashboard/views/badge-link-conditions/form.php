ec1b27c82dc72673067252b121bb6da0f7691502
ablyanup <anup.rawat@ablysoft.com>
2022-07-19 18:53:32 +0530

    bug-065453 image preview is not proper

--- dashboard/views/badge-link-conditions/form.php
+++ dashboard/views/badge-link-conditions/form.php
@@ -53,18 +53,16 @@ if (Badge::TYPE_BADGE == $badgeType) {
                 <div class="row justify-content-between">
                     <div class="col-lg-4 badgeImageSection--js">
                         <?php if (Badge::TYPE_BADGE == $badgeType) { ?>
-                            <div class=" badge-image" data-ratio="1:1">
+                            <div class="badge-image">
                                 <?php echo $imageHtml; ?>
-                                <img src="<?php echo CONF_WEBROOT_FRONTEND; ?>images/defaults/product_default_image.jpg">
                             </div>
                         <?php } else { ?>
-                            <div class=" badge-image" data-ratio="1:1">
-                                <div class="products">
-                                    <div class="products-body"><?php echo $imageHtml; ?>
-                                        <img src="<?php echo CONF_WEBROOT_FRONTEND; ?>images/defaults/product_default_image.jpg">
-                                    </div>
+                            <div class="products">
+                                <div class="products-body"><?php echo $imageHtml; ?>
+                                    <img src="<?php echo CONF_WEBROOT_FRONTEND; ?>images/defaults/product_default_image.jpg">
                                 </div>
                             </div>
+
                         <?php } ?>
                     </div>
                     <div class="col-lg-8">
