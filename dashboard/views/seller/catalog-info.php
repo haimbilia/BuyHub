51a5008f049cbee3f9174659d7132e6f22ab37f5
ablyshashi <shashi.kumar@fatbit.in>
2022-08-08 13:09:16 +0530

    bug-064809 - Shop Details Page >> Multiple UI issues in the filters section on the shop details page.

--- dashboard/views/seller/catalog-info.php
+++ dashboard/views/seller/catalog-info.php
@@ -28,32 +28,32 @@
                 <div class="mt-4">
                     <ul class="list-stats list-stats-double">
                         <li class="list-stats-item">
-                            <span class="lable"><?php echo Labels::getLabel('LBL_Category', $siteLangId); ?>:</span>
+                            <span class="lable"><?php echo Labels::getLabel('LBL_Category', $siteLangId); ?></span>
                             <span class="value"><?php echo $product['prodcat_name']; ?></span>
                         </li>
                         <li class="list-stats-item">
-                            <span class="lable"><?php echo Labels::getLabel('LBL_Brand', $siteLangId); ?>:</span>
+                            <span class="lable"><?php echo Labels::getLabel('LBL_Brand', $siteLangId); ?></span>
                             <span class="value"><?php echo ($product['brand_name']) ? $product['brand_name'] : Labels::getLabel('LBL_N/A', $siteLangId); ?></span>
                         </li>
                         <li class="list-stats-item">
-                            <span class="lable"><?php echo Labels::getLabel('LBL_Product_Model', $siteLangId); ?>:</span>
+                            <span class="lable"><?php echo Labels::getLabel('LBL_Product_Model', $siteLangId); ?></span>
                             <span class="value"><?php echo $product['product_model']; ?></span>
                         </li>
                         <li class="list-stats-item">
-                            <span class="lable"><?php echo Labels::getLabel('LBL_Minimum_Selling_Price', $siteLangId); ?>:</span>
+                            <span class="lable"><?php echo Labels::getLabel('LBL_Minimum_Selling_Price', $siteLangId); ?></span>
                             <span class="value"><?php echo CommonHelper::displayMoneyFormat($product['product_min_selling_price']); ?></span>
                         </li>
                         <?php $saleTaxArr = Tax::getSaleTaxCatArr($siteLangId);
                         if (isset($product['ptt_taxcat_id']) && array_key_exists($product['ptt_taxcat_id'], $saleTaxArr)) { ?>
                             <li class="list-stats-item">
-                                <span class="lable"><?php echo Labels::getLabel('LBL_Tax_Category', $siteLangId); ?>:</span>
+                                <span class="lable"><?php echo Labels::getLabel('LBL_Tax_Category', $siteLangId); ?></span>
                                 <span class="value"><?php echo $saleTaxArr[$product['ptt_taxcat_id']]; ?></span>
                             </li>
                         <?php } ?>
                     </ul>
                     <?php if (count($productSpecifications) > 0) { ?>
                         <div class="mt-4">
-                            <div class="h6"><?php echo Labels::getLabel('LBL_Specifications', $siteLangId); ?>:</div>
+                            <div class="h6"><?php echo Labels::getLabel('LBL_Specifications', $siteLangId); ?></div>
                             <div class="list list--specification">
                                 <ul class="list-stats list-stats-double">
                                     <?php $count = 1;
@@ -62,7 +62,7 @@
                                             continue;
                                         } ?>
                                         <li class="list-stats-item">
-                                            <span class="lable"><?php echo $specification['prodspec_name']; ?>:</span>
+                                            <span class="lable"><?php echo $specification['prodspec_name']; ?></span>
                                             <span class="value"><?php echo $specification['prodspec_value']; ?></span>
                                         </li>
 
