0d1f6de20cbc4ff19e0ff1f26ffe70576b5f97f9
ablyanup <anup.rawat@ablysoft.com>
2022-08-16 13:57:01 +0530

    task-93086 restrore offcanvas id updated

--- application/views/restore-system/top-header.php
+++ application/views/restore-system/top-header.php
@@ -1,10 +1,10 @@
- <button class="restore-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight">
+ <button class="restore-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRestore">
      <svg class="svg" width="32" height="32">
          <use xlink:href="<?php echo CONF_WEBROOT_FRONTEND; ?>images/retina/sprite-restore.svg#gear">
          </use>
      </svg>
  </button>
- <div class="offcanvas offcanvas-end offcanvas-restore" tabindex="-1" id="offcanvasRight">
+ <div class="offcanvas offcanvas-end offcanvas-restore" tabindex="-1" id="offcanvasRestore">
      <div class="offcanvas-body">
          <div class="demo">
              <div class="demo-restore">
