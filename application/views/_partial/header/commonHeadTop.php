c8996282d88845a86f9eb5f7db665787ce574f64
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2022-08-23 12:28:51 +0530

    task-98420 Updated meta tag for zoom in ios

--- application/views/_partial/header/commonHeadTop.php
+++ application/views/_partial/header/commonHeadTop.php
@@ -11,14 +11,16 @@ if ($controllerName == 'Products' && $actionName == 'view') {
 $additionalAttributes = (CommonHelper::getLayoutDirection() == 'rtl') ? 'direction="rtl" style="direction: rtl;"' : '';
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo strtolower(CommonHelper::getLangCode()); ?>" data-version="<?php echo CONF_WEB_APP_VERSION; ?>" data-theme="light" dir="<?php echo CommonHelper::getLayoutDirection(); ?>" prefix="og: http://ogp.me/ns#" <?php echo $additionalAttributes; ?> class="<?php echo $htmlClass; ?> <?php if (FatApp::getConfig('CONF_AUTO_RESTORE_ON', FatUtility::VAR_INT, 1) && CommonHelper::demoUrl()) { echo "sticky-demo-header"; } ?>">
+<html lang="<?php echo strtolower(CommonHelper::getLangCode()); ?>" data-version="<?php echo CONF_WEB_APP_VERSION; ?>" data-theme="light" dir="<?php echo CommonHelper::getLayoutDirection(); ?>" prefix="og: http://ogp.me/ns#" <?php echo $additionalAttributes; ?> class="<?php echo $htmlClass; ?> <?php if (FatApp::getConfig('CONF_AUTO_RESTORE_ON', FatUtility::VAR_INT, 1) && CommonHelper::demoUrl()) {
+                                                                                                                                                                                                                                                                                                            echo "sticky-demo-header";
+                                                                                                                                                                                                                                                                                                        } ?>">
 
 <head>
     <!-- Yo!Kart -->
     <meta charset="utf-8">
     <meta name="author" content="">
     <!-- Mobile Specific Metas ===================== -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <?php if (CommonHelper::demoUrl() && $controllerName != 'Home') { ?>
         <meta name="robots" content="noindex" />
     <?php } ?>
