6a8194ff618478734c2eb1de56a1dcf9bda722ad
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-08-24 10:12:13 +0530

    task-94791-Forgot Password Admin sms template

--- admin/controllers/AdminGuestController.php
+++ admin/controllers/AdminGuestController.php
@@ -127,7 +127,7 @@ class AdminGuestController extends FatController
         }
 
         $emaiHandObj = new EmailHandler();
-        $emaiHandObj->sendSms('admin_forgot_password', FatApp::getConfig('CONF_SITE_PHONE'), $replacements, $this->siteLangId);
+        $emaiHandObj->sendSms('admin_forgot_password', ValidateElement::formatDialCode(FatApp::getConfig('CONF_SITE_PHONE_dcode')) . FatApp::getConfig('CONF_SITE_PHONE'), $replacements, $this->siteLangId);
 
         $this->set('msg', Labels::getLabel('MSG_YOUR_PASSWORD_RESET_INSTRUCTIONS_TO_YOUR_EMAIL', $this->siteLangId));
         $this->_template->render(false, false, 'json-success.php', true, false);
@@ -178,7 +178,7 @@ class AdminGuestController extends FatController
             LibHelper::exitWithError(Labels::getLabel('ERR_UNABLE_TO_SEND_EMAIL', $this->siteLangId), true);
         }
         $emaiHandObj = new EmailHandler();
-        $emaiHandObj->sendSms('admin_forgot_password', FatApp::getConfig('CONF_SITE_PHONE'), $replacements, $this->siteLangId);
+        $emaiHandObj->sendSms('admin_forgot_password', ValidateElement::formatDialCode(FatApp::getConfig('CONF_SITE_PHONE_dcode')) . FatApp::getConfig('CONF_SITE_PHONE'), $replacements, $this->siteLangId);
 
         $this->set('msg', Labels::getLabel('MSG_YOUR_PASSWORD_RESET_INSTRUCTIONS_TO_YOUR_EMAIL', $this->siteLangId));
         $this->_template->render(false, false, 'json-success.php', true, false);
