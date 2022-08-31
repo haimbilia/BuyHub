d039739ca1b3e504adce3c22f3978bacc63df114
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2022-08-31 18:45:38 +0530

    task-98420 worked on empty cart view

--- application/views/_partial/no-record-found.php
+++ application/views/_partial/no-record-found.php
@@ -1,12 +1,14 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
 <div class="block-empty text-center">
     <img class="block__img" src="<?php echo CONF_WEBROOT_URL; ?>images/empty_item.svg" alt="<?php echo Labels::getLabel('LBL_No_record_found', $siteLangId); ?>" width="150px">
-    <h5><?php
+    <h3>
+        <?php
         if (isset($message)) {
             echo $message;
         } else {
             echo Labels::getLabel('LBL_No_record_found', $siteLangId);
-        } ?></h5>
+        } ?>
+    </h3>
     <?php if (!empty($linkArr)) {
         foreach ($linkArr as $link) {
             $onClick = isset($link['onClick']) ? "onClick='" . $link['onClick'] . "'" : "";
