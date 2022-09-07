c8b5a49fe125ccadd20dafc61e480e71e2bff0fb
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-09-07 13:44:11 +0530

    task-94791-Update db query

--- database/update.sql
+++ database/update.sql
@@ -2218,4 +2218,4 @@ ON DUPLICATE KEY UPDATE label_caption = VALUES(label_caption);
 -- -----------------TV-9.4.0.20220907--------------------------
 
 ALTER TABLE `tbl_badges` ADD `badge_text_color` VARCHAR(150) NOT NULL AFTER `badge_color`;
-ALTER TABLE `tbl_badges` CHANGE `badge_display_inside` `badge_display_inside` TINYINT NOT NULL COMMENT 'For Ribbons', CHANGE `badge_shape_type` `badge_shape_type` INT NOT NULL COMMENT 'For Ribbons', CHANGE `badge_color` `badge_color` VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'For Ribbons', CHANGE `badge_text_color` `badge_text_color` VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'For Ribbons';
+ALTER TABLE `tbl_badges` CHANGE `badge_display_inside` `badge_display_inside` TINYINT NOT NULL COMMENT 'For Ribbons', CHANGE `badge_shape_type` `badge_shape_type` INT NOT NULL COMMENT 'For Ribbons', CHANGE `badge_color` `badge_color` VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'For Ribbons', CHANGE `badge_text_color` `badge_text_color` VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'For Ribbons';
