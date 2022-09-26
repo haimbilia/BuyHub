5685ddb50360bb0fe858dd81833ecea8e163cc77
Satbir Kaushik <satbir.kaushik@fatbit.in>
2022-09-26 10:36:21 +0530

    task-95487-Cc avenue payment gateway requiements comments

--- library/plugins/payment-methods/ccavenue/Ccavenue.php
+++ library/plugins/payment-methods/ccavenue/Ccavenue.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * CCAvenue Requirements:
+ * php mcrypt extension need to be install on server level
+ * Different Paymentgateway production and sanbox url based on country for which it is used.
+ * API credentials
+ *  - Create account according to your country (India, UAE etc.)
+ *  - Connect with CCAvenue team to complete merchant account.
+ *  - Copy keys from  > Settings > API Keys.
+ * NOTE: You need to connect with CC Avenue team over phone call or email to MerchantUnderwriting@ccavenue.com to add another test URL if required. 
+ */
 
 class Ccavenue extends PaymentMethodBase
 {
