7f31ec6aab939a02d67189e7567b9d5135d2bb22
Satbir Kaushik <satbir.kaushik@fatbit.in>
2023-05-03 15:55:08 +0530

    Bug-072778 - Coupon savings are not coming proper

--- application/mobile-views/cart/price-detail.php
+++ application/mobile-views/cart/price-detail.php
@@ -119,8 +119,8 @@ $priceDetail['netPayable'] = array(
     'value' => CommonHelper::displayMoneyFormat($orderNetAmount)
 );
 
-$data['cartSummary']['cartDiscounts'] = !empty($data['cartSummary']['cartDiscounts']) ? $data['cartSummary']['cartDiscounts'] : (object)array();
 if (!empty($data['cartSummary']['cartDiscounts'])) {
     $data['cartSummary']['cartDiscounts']['coupon_discount_total'] = CommonHelper::displayMoneyFormat($data['cartSummary']['cartDiscounts']['coupon_discount_total']);
 }
+$data['cartSummary']['cartDiscounts'] = !empty($data['cartSummary']['cartDiscounts']) ? $data['cartSummary']['cartDiscounts'] : (object)array();
 $data = !empty($data) ? array_merge($data, $priceDetail) : $priceDetail;
