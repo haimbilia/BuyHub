300956df0d219edc0d227c8771cf2d61658268c3
Satbir Kaushik <satbir.kaushik@fatbit.in>
2023-06-07 17:35:36 +0530

    task-103082-Showing error on ppc promotion listing if that promotion belongs to advertiser only

--- admin/views/_partial/shop/shop-info-card.php
+++ admin/views/_partial/shop/shop-info-card.php
@@ -1,17 +1,16 @@
-<?php
-defined('SYSTEM_INIT') or die('Invalid Usage.');
-
+<?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 $uploadedTime = AttachedFile::setTimeParam($shop['shop_updated_on']);
-$imgSrc = UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'shopLogo', array($shop['shop_id'], $siteLangId, ImageDimension::VIEW_THUMB), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
+$shopId = (int)$shop['shop_id'];
+$imgSrc = UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'shopLogo', array($shopId, $siteLangId, ImageDimension::VIEW_THUMB), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
 $getShopAspectRatio = ImageDimension::getData(ImageDimension::TYPE_SHOP_LOGO, ImageDimension::VIEW_THUMB);
-$onclick = !empty($onclick) ? "onclick = '" . $onclick . "'" : "onclick = 'redirectToShop(" . $shop['shop_id'] . ")'";
+$onclick = !empty($onclick) ? "onclick = '" . $onclick . "'" : "onclick = 'redirectToShop(" . $shopId . ")'";
 $useFeatherLightJs = $useFeatherLightJs ?? 0;
 $showImage = $showImage ?? true;
 
 $imgOrgUrl = 'javascript:void(0)';
 $cls = 'product-profile';
 if (1 == $useFeatherLightJs) {
-    $imgOrgUrl = UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'shopLogo', array((int)$shop['shop_id'], $siteLangId, ImageDimension::VIEW_ORIGINAL), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
+    $imgOrgUrl = UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'shopLogo', array($shopId, $siteLangId, ImageDimension::VIEW_ORIGINAL), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.jpg');
     $onclick = 'data-featherlight="image"';
     $cls .= ' featherLightJs';
 }
@@ -29,8 +28,15 @@ if (1 == $useFeatherLightJs) {
         <?php if (!empty($shop['user_name'])) { ?>
             <ul class="list-options <?php echo isset($horizontalAlignOptions) && $horizontalAlignOptions ? 'list-options--horizontal' : 'list-options--vertical"'; ?>">
                 <li>
-                    <span class="label"><?php echo Labels::getLabel('LBL_SELLER', $siteLangId); ?>:</span>
-                    <a href="javascript:void();" onclick="redirectUser(<?php echo $shop['shop_user_id']; ?>);" class="value"><?php echo $shop['user_name']; ?></a>
+                    <span class="label">
+                        <?php 
+                        $lbl = Labels::getLabel('LBL_SELLER', $siteLangId);
+                        if (1 > $shopId && 0 < $shop['user_is_advertiser']) {
+                            $lbl = Labels::getLabel('LBL_ADVERTISER', $siteLangId);
+                        } ?>
+                        <?php echo $lbl; ?>:
+                    </span>
+                    <a href="javascript:void();" onclick="redirectUser(<?php echo $shop['user_id'] ?? $shop['shop_user_id']; ?>);" class="value"><?php echo $shop['user_name']; ?></a>
                 </li>
             </ul>
         <?php } ?>
