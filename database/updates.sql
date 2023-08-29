db003163781c560537f2f40e1a4342d3484982c0
Satbir Kaushik <satbir.kaushik@fatbit.in>
2023-08-29 16:12:56 +0530

    task-104474-Queries logged for app labels

---
+++ database/updates.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO `tbl_language_labels` ( `label_key`, `label_lang_id`, `label_caption`, `label_type`) VALUES 
+('APP_ASK_A_QUESTIONS', 1, 'Ask a question', 2),
+('APP_ASK_QUESTIONS', 1, 'Ask a question', 2)
+ON DUPLICATE KEY UPDATE label_caption = VALUES(label_caption);
