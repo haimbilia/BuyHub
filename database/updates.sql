e996d833a7b29c66f3e58e2cdcccb971364e3d2a
Sanchit Sharma <sanchit.sharma@fatbit.in>
2023-09-28 17:42:47 +0530

    bug-076198 Unable to save downloadable file on new digital product, removed update query

--- database/updates.sql
+++ database/updates.sql
@@ -2,4 +2,3 @@ INSERT IGNORE INTO `tbl_language_labels` ( `label_key`, `label_lang_id`, `label_
 ('APP_ASK_A_QUESTIONS', 1, 'Ask a question', 2),
 ('APP_ASK_QUESTIONS', 1, 'Ask a question', 2)
 ON DUPLICATE KEY UPDATE label_caption = VALUES(label_caption);
-ALTER TABLE `tbl_product_digital_data_relation` ADD `pddr_updated_on` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `pddr_type`;
