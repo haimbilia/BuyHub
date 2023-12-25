3a81e5c3c0a3cb50759e9cf90510fd03125271f1
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2023-12-25 13:48:44 +0530

    task-115727 fixed gift card form UI

--- dashboard/views/buyer/gift-card-form.php
+++ dashboard/views/buyer/gift-card-form.php
@@ -17,80 +17,62 @@ $submitField->addFieldTagAttribute('class', 'btn btn-brand btn-wide ');
     </h5>
 </div>
 <div class="modal-body">
-    <div class="form-edit-body">
+    <div class="selection">
+        <?php echo $form->getFormTag(); ?>
+        <div class="row justify-content-between">
+            <div class="col-xl-6">
+                <div class="field-set">
+                    <label class="field_label margin-bottom-2">
+                        <?php echo $amount->getCaption(); ?>
+                        <?php if ($amount->requirement->isRequired()) { ?>
+                        <span class="spn_must_field">*</span>
+                        <?php  } ?>
+                    </label>
+                    <?php echo $amount->getHTML(); ?>
+                </div>
 
-        <div class="facebox-panel">
+                <div class="field-set">
+                    <label class="field_label margin-bottom-2">
+                        <?php echo $receiverName->getCaption(); ?>
+                        <?php if ($receiverName->requirement->isRequired()) { ?>
+                        <span class="spn_must_field">*</span>
+                        <?php  } ?>
+                    </label>
+                    <?php echo $receiverName->getHTML(); ?>
+                </div>
 
-            <div class="facebox-panel__body padding-bottom-0">
-                <div class="selection selection--checkout selection--payment">
-                    <?php echo $form->getFormTag(); ?>
-                    <div class="row justify-content-between">
-                        <div class="col-md-6 col-xl-6">
-                            <div class="field-set">
-                                <label class="field_label margin-bottom-2">
-                                    <?php echo $amount->getCaption(); ?>
-                                    <?php if ($amount->requirement->isRequired()) { ?>
-                                        <span class="spn_must_field">*</span>
-                                    <?php  } ?>
-                                </label>
-                                <?php echo $amount->getHTML(); ?>
-                            </div>
-
-                            <div class="field-set">
-                                <label class="field_label margin-bottom-2">
-                                    <?php echo $receiverName->getCaption(); ?>
-                                    <?php if ($receiverName->requirement->isRequired()) { ?>
-                                        <span class="spn_must_field">*</span>
-                                    <?php  } ?>
-                                </label>
-                                <?php echo $receiverName->getHTML(); ?>
-                            </div>
-
-                            <div class="field-set">
-                                <label class="field_label margin-bottom-2">
-                                    <?php echo $receiverEmail->getCaption(); ?>
-                                    <?php if ($receiverEmail->requirement->isRequired()) { ?>
-                                        <span class="spn_must_field">*</span>
-                                    <?php  } ?>
-                                </label>
-                                <?php echo $receiverEmail->getHTML(); ?>
-                            </div>
-
-                        </div>
-
-                        <div class="col-md-6 col-xl-6">
-                            <div class="selection-title">
-                                <label class="field_label margin-bottom-2"><?php echo Labels::getLabel('LBL_PAYMENT_METHOD'); ?> <span class="spn_must_field">*</span></label>
-                            </div>
-                            <div class="step">
-                                <div class="step_section">
-                                    <div class="step_body">
-                                        <div id="payment">
-                                            <div class="payment-area">
-                                                <div class="payments-nav" id="payment_methods_tab">
-                                                    <?php
-                                                    echo    $pmethodField->getHTML();
-                                                    ?>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php
-                            if (!empty($paymentMethods)) {
-                                echo $submitField->getHTML();
-                            } else {
-                                echo Labels::getLabel('LBL_PLEASE_ENABLED_PAYMENT_METHODS_OR_ADD_MONEY_IN_WALLET');
-                            }
-                            ?>
-                        </div>
-                    </div>
-                    </form>
+                <div class="field-set">
+                    <label class="field_label margin-bottom-2">
+                        <?php echo $receiverEmail->getCaption(); ?>
+                        <?php if ($receiverEmail->requirement->isRequired()) { ?>
+                        <span class="spn_must_field">*</span>
+                        <?php  } ?>
+                    </label>
+                    <?php echo $receiverEmail->getHTML(); ?>
                 </div>
             </div>
+            <div class="col-xl-6">
+                <div class="selection-title">
+                    <label class="field_label margin-bottom-2"><?php echo Labels::getLabel('LBL_PAYMENT_METHOD'); ?> <span class="spn_must_field">*</span></label>
+                </div>
+                <div class="payment-area" id="payment">
+                    <div class="payments-nav" id="payment_methods_tab">
+                        <?php
+                        echo  $pmethodField->getHTML();
+                        ?>
+                    </div>
+                </div>
+                <?php
+                if (!empty($paymentMethods)) {
+                    echo $submitField->getHTML();
+                } else {
+                    echo Labels::getLabel('LBL_PLEASE_ENABLED_PAYMENT_METHODS_OR_ADD_MONEY_IN_WALLET');
+                }
+                ?>
+            </div>
         </div>
+        </form>
     </div>
 </div>
+
 <?php echo $form->getExternalJS(); ?>
