db66a545c812f0660bbd656f5eb41e01aa801237
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2023-12-25 14:54:47 +0530

    task-115727 fix

--- dashboard/views/buyer/gift-card-form.php
+++ dashboard/views/buyer/gift-card-form.php
@@ -19,13 +19,13 @@ $submitField->addFieldTagAttribute('class', 'btn btn-brand btn-wide');
 </div>
 <?php echo $form->getFormTag(); ?>
 <div class="modal-body">
-    <div class="row justify-content-between">
-        <div class="col-md-12 col-xl-12">
+    <div class="row">
+        <div class="col-md-12">
             <div class="field-set">
                 <label class="form-label">
                     <?php echo $amount->getCaption(); ?>
                     <?php if ($amount->requirement->isRequired()) { ?>
-                        <span class="spn_must_field">*</span>
+                    <span class="spn_must_field">*</span>
                     <?php  } ?>
                 </label>
                 <?php echo $amount->getHTML(); ?>
@@ -34,7 +34,7 @@ $submitField->addFieldTagAttribute('class', 'btn btn-brand btn-wide');
                 <label class="form-label">
                     <?php echo $receiverName->getCaption(); ?>
                     <?php if ($receiverName->requirement->isRequired()) { ?>
-                        <span class="spn_must_field">*</span>
+                    <span class="spn_must_field">*</span>
                     <?php  } ?>
                 </label>
                 <?php echo $receiverName->getHTML(); ?>
@@ -43,24 +43,13 @@ $submitField->addFieldTagAttribute('class', 'btn btn-brand btn-wide');
                 <label class="form-label">
                     <?php echo $receiverEmail->getCaption(); ?>
                     <?php if ($receiverEmail->requirement->isRequired()) { ?>
-                        <span class="spn_must_field">*</span>
+                    <span class="spn_must_field">*</span>
                     <?php  } ?>
                 </label>
                 <?php echo $receiverEmail->getHTML(); ?>
             </div>
-
         </div>
     </div>
-    <p class="note">
-        <?php
-        if (!empty($paymentMethods)) {
-            echo $submitField->getHTML();
-        } else {
-            echo Labels::getLabel('LBL_PLEASE_ENABLED_PAYMENT_METHODS_OR_ADD_MONEY_IN_WALLET');
-        }
-        ?>
-    </p>
-
 </div>
 <div class="modal-footer">
     <?php
