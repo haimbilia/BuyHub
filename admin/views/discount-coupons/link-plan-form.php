632507615708cb377c050eadd4956800c256e720
Anup Rawat <anup.rawat@ablysoft.com>
2024-07-09 16:55:04 +0530

    task-121674 link sellers to discount copon for packages

--- admin/views/discount-coupons/link-plan-form.php
+++ admin/views/discount-coupons/link-plan-form.php
@@ -3,7 +3,7 @@
 $fld = $frm->getField('plan_name');
 $fld->addFieldTagAttribute('class', 'tagifyJs');
 $fld->addFieldTagAttribute('data-record-id', $recordId);
-
+$frm->addHtml('','','<a href="'.UrlHelper::generateUrl('DiscountCoupons', 'links', [$recordId]).'">'.Labels::getlabel('LBL_CLICK_TO_LINK_SELLERS').'</a>');
 require_once(CONF_THEME_PATH . '_partial/listing/form.php'); ?>
 
 <script>
