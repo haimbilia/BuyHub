c6b92a51b49133453ecbb24c04b498a173fefe98
Anup Rawat <anup.rawat@ablysoft.com>
2024-07-30 16:01:06 +0530

    bug-085804 Service product quantity appears as zero on the product details page

--- dashboard/utilities/traits/SellerProducts.php
+++ dashboard/utilities/traits/SellerProducts.php
@@ -550,9 +550,10 @@ trait SellerProducts
         $post['selprod_track_inventory'] = FatApp::getPostedData('selprod_track_inventory', FatUtility::VAR_INT, 0);
         $post['selprod_min_order_qty'] = FatApp::getPostedData('selprod_min_order_qty', FatUtility::VAR_INT, 1);
         $post['selprod_stock'] = FatApp::getPostedData('selprod_stock', FatUtility::VAR_INT, 1);
+        $post['selprod_stock'] = ($post['selprod_stock'] > 0) ? $post['selprod_stock'] : 1;
 
         $keywordSlug = '';
-        $productId = SellerProduct::getAttributesById($selprod_id, 'selprod_product_id', false);
+        $productId = SellerProduct::getAttributesById($selprod_id, 'selprod_product_id', false);       
         if (empty($post['selprod_title' . $this->siteLangId])) {
             $productLangRow = Product::getProductDataById($this->siteLangId, $productId, array('product_identifier', 'product_name'));
             $keywordSlug = $productLangRow['product_name'] ?? $productLangRow['product_identifier'];
