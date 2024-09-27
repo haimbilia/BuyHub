f6fce349f80d53e135c1226d77f56d91cb798ab0
Satbir <satbir.kaushik@fatbit.in>
2024-09-27 11:00:31 +0530

    task-124268-Search gift card API response updated

--- dashboard/mobile-views/buyer/search-gift-cards.php
+++ dashboard/mobile-views/buyer/search-gift-cards.php
@@ -1,5 +1,9 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
+foreach ($arrListing as &$row) {
+    $row['order_net_amount'] = CommonHelper::displayMoneyFormat($row['order_net_amount']);
+}
+
 $data = array(
     'giftCards' => $arrListing,
     'orderPaymentStatusArr' => $orderPaymentStatusArr,
