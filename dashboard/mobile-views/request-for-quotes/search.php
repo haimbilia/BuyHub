84c32d7d413b7358f5afee7cb1d76c8d82585687
Satbir <satbir.kaushik@fatbit.in>
2024-10-03 16:14:17 +0530

    task-124268-RFQ search listing with product image

--- dashboard/mobile-views/request-for-quotes/search.php
+++ dashboard/mobile-views/request-for-quotes/search.php
@@ -6,6 +6,7 @@ if (!empty($arrListing)) {
         if ($isBuyer && 0 < $rfq['acceptedOffers']) {
             $rfq['downloadRfqCopyUrl'] = UrlHelper::generateFullUrl('RequestForQuotes', 'downloadRfqCopy', [$rfq['rfq_id']]); 
         }
+        $rfq['product_image'] = UrlHelper::getCachedUrl(UrlHelper::generateFullFileUrl('Image', 'product', array($rfq['rfq_product_id'], ImageDimension::VIEW_MEDIUM, $rfq['rfq_selprod_id'], 0), CONF_WEBROOT_FRONTEND), CONF_IMG_CACHE_TIME, '.jpg');
     }
 }
 
