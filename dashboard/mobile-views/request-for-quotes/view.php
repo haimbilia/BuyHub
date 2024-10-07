bcb54a62ba88783129352e9ae430e11aa5dbcffe
Jatinder Singh <jatinder.singh@fatbit.in>
2024-10-07 14:47:05 +0530

    task-124268 : RFQ view API added param attachment_url.

--- dashboard/mobile-views/request-for-quotes/view.php
+++ dashboard/mobile-views/request-for-quotes/view.php
@@ -1,5 +1,14 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
+$url = '';
+$res = AttachedFile::getAttachment(AttachedFile::FILETYPE_RFQ, $recordId, 0, langId: -1);
+
+if (!empty($res) && 0 < $res['afile_id']) {
+    $url = UrlHelper::generateFullUrl('RequestForQuotes', 'downloadFile', array($recordId));
+}
+
+$rfqData['attachment_url'] = $url;
+
 $data = array(
     'statusArr' => $statusArr,
     'approvalStatusArr' => $approvalStatusArr,
