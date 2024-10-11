df4438318cd50752cbced5b2a9a28f5d68198144
Satbir <satbir.kaushik@fatbit.in>
2024-10-11 15:29:29 +0530

    task-122685-new banner layout 4 collection

--- application/utilities/ImageDimension.php
+++ application/utilities/ImageDimension.php
@@ -808,6 +808,15 @@ class ImageDimension extends FatUtility
             ];
         } */
 
+        if ($layout == Collections::TYPE_BANNER_LAYOUT4) {
+            $arr =  [
+                self::VIEW_DESKTOP => [self::WIDTH => 2000, self::HEIGHT => 666],
+                self::VIEW_MOBILE => [self::WIDTH => 640, self::HEIGHT => 360],
+                self::VIEW_TABLET => [self::WIDTH => 1024, self::HEIGHT => 576],
+                self::VIEW_THUMB => [self::WIDTH => 200, self::HEIGHT => 112],
+            ];
+        } 
+
         if (empty($layout)) {
             $arr =  [
                 self::VIEW_DESKTOP => [self::WIDTH => 2000, self::HEIGHT => 666],
