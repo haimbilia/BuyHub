d82a0e25e25204440bff271bc9e6ec83349f1651
Anup Rawat <anup.rawat@ablysoft.com>
2024-10-14 12:46:36 +0530

    task-103589 RTL dropdown issue

--- application/views/_partial/footerNavigation.php
+++ application/views/_partial/footerNavigation.php
@@ -1,19 +1,22 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
-if (!empty($footer_navigation)) { ?>
-    <?php foreach ($footer_navigation as $nav) { ?>
+if (!empty($footer_navigation)) {
+    foreach ($footer_navigation as $i => $nav) { ?>
         <div class="col-lg-4 col-md-4 mb-3 mb-md-0">
             <div class="footer-group">
-                <h4 class="footer-group-head js-footer-group-head dropdown-toggle-custom">
+                <h4 class="footer-group-head dropdown-toggle-custom" data-bs-toggle="collapse"
+                    data-bs-target="#collapseWidth<?php echo $i; ?>" role="button" aria-expanded="false" aria-controls="collapseExample">
                     <?php echo $nav['parent']; ?><i class="dropdown-toggle-custom-arrow"></i>
                 </h4>
-                <ul class="footer-nav js-footer-nav">
+                <ul class="footer-nav collapse" id="collapseWidth<?php echo $i; ?>">
                     <?php if ($nav['pages']) {
                         $getOrgUrl = (CONF_DEVELOPMENT_MODE) ? true : false;
                         foreach ($nav['pages'] as $link) {
                             $navUrl = CommonHelper::getnavigationUrl($link['nlink_type'], $link['nlink_url'], $link['nlink_cpage_id'], $link['nlink_category_id']);
                             $OrgnavUrl = CommonHelper::getnavigationUrl($link['nlink_type'], $link['nlink_url'], $link['nlink_cpage_id'], $link['nlink_category_id'], $getOrgUrl); ?>
                             <li class="footer-nav-item">
-                                <a class="footer-nav-link" target="<?php echo $link['nlink_target']; ?>" data-org-url="<?php echo $OrgnavUrl; ?>" href="<?php echo $navUrl; ?>"><?php echo $link['nlink_caption']; ?>
+                                <a class="footer-nav-link" target="<?php echo $link['nlink_target']; ?>"
+                                    data-org-url="<?php echo $OrgnavUrl; ?>"
+                                    href="<?php echo $navUrl; ?>"><?php echo $link['nlink_caption']; ?>
                                 </a>
                             </li>
                     <?php }
