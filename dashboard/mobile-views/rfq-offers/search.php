28d73fdd308a59ddfad41d18eb771fbeeec5db35
Satbir <satbir.kaushik@fatbit.in>
2024-10-15 16:32:39 +0530

    task-122685-Added isBought extra param in rfq offer search API

--- dashboard/mobile-views/rfq-offers/search.php
+++ dashboard/mobile-views/rfq-offers/search.php
@@ -1,5 +1,9 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
+foreach ($arrListing as &$value) {
+    $value['isBought'] = RfqOffers::isBought($value['rlo_accepted_offer_id']) ? 1 : 0;
+}
+
 $data = array(
     'offersCountArr' => array_values($offersCountArr),
     'statusArr' => $statusArr,
