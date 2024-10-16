594858bc181a154068fc8a7839bb39eab54777f6
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2024-10-16 17:51:32 +0530

    task-120303 worked on home page header UI

--- application/views/_partial/topHeader.php
+++ application/views/_partial/topHeader.php
@@ -1,11 +1,12 @@
 <div class="wrapper">
     <?php if (FatApp::getConfig('CONF_LOADER', FatUtility::VAR_INT, 0)) { ?>
-        <div class="page-loader">
-            <span><?php echo Labels::getLabel('LBL_Loading...'); ?><i class="loader-line"></i></span>
-        </div>
+    <div class="page-loader">
+        <span><?php echo Labels::getLabel('LBL_Loading...'); ?><i class="loader-line"></i></span>
+    </div>
     <?php } ?>
     <!--header start here-->
-    <header id="header" class="header <?php echo (FatApp::getConfig('CONF_HEADER_FULL_WIDTH', FatUtility::VAR_INT, 1) ? 'fluid' : '') ?> no-print">
+    <header id="header"
+        class="header <?php echo (FatApp::getConfig('CONF_HEADER_FULL_WIDTH', FatUtility::VAR_INT, 1) ? 'fluid' : '') ?> no-print">
         <?php if (FatApp::getConfig('CONF_AUTO_RESTORE_ON', FatUtility::VAR_INT, 1) && CommonHelper::demoUrl()) {
             $this->includeTemplate('restore-system/top-header.php');
         } ?>
@@ -16,6 +17,14 @@
                         <?php $this->includeTemplate('_partial/footerNavigation.php', ['topHeadLinks' => true]); ?>
                     </div>
                     <div class="black-bar-end">
+                        <?php 
+                        if (CommonHelper::demoUrl()) { ?>
+                        <a class="btn-cta-outline" href="https://www.yo-kart.com/contact-us.html?demo-cta"
+                            rel="noopener" target="_blank"
+                            title="Connect with Yo!Kart team to build a Multivendor Marketplace">Start Your
+                            Marketplace</a>
+
+                        <?php }?>
                         <?php $this->includeTemplate('_partial/headerLanguageArea.php'); ?>
                     </div>
                 </div>
@@ -47,10 +56,10 @@
                         }
 
                         if (FatApp::getConfig('CONF_ENABLE_GEO_LOCATION', FatUtility::VAR_INT, 0) && !empty(FatApp::getConfig('CONF_GOOGLEMAP_API_KEY', FatUtility::VAR_STRING, '')) && $diplayGeoLocation) { ?>
-                            <div class="geo-location">
-                                <div class="geo-location_inner">
-                                    <div class="dropdown">
-                                        <?php
+                        <div class="geo-location">
+                            <div class="geo-location_inner">
+                                <div class="dropdown">
+                                    <?php
                                         $geoAddress = '';
                                         if ((!isset($_COOKIE['_ykGeoLat']) || !isset($_COOKIE['_ykGeoLng']) || !isset($_COOKIE['_ykGeoCountryCode'])) && FatApp::getConfig('CONF_DEFAULT_GEO_LOCATION', FatUtility::VAR_INT, 0)) {
                                             $geoAddress = FatApp::getConfig('CONF_GEO_DEFAULT_ADDR', FatUtility::VAR_STRING, '');
@@ -63,22 +72,22 @@
                                         }
                                         $geoAddress =  isset($_COOKIE["_ykGeoAddress"]) ? $_COOKIE["_ykGeoAddress"] : $geoAddress;
                                         ?>
-                                        <button class="button-geo-location geo-location_trigger" type="button"
-                                            onclick="setGeoLocation()">
+                                    <button class="button-geo-location geo-location_trigger" type="button"
+                                        onclick="setGeoLocation()">
 
-                                            <svg class="svg" width="18" height="18">
-                                                <use
-                                                    xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#gps">
-                                                </use>
-                                            </svg>
+                                        <svg class="svg" width="18" height="18">
+                                            <use
+                                                xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#gps">
+                                            </use>
+                                        </svg>
 
-                                            <div class="geo-location-selected">
-                                                <?php echo $geoAddress; ?>
-                                            </div>
-                                        </button>
-                                    </div>
+                                        <div class="geo-location-selected">
+                                            <?php echo $geoAddress; ?>
+                                        </div>
+                                    </button>
                                 </div>
                             </div>
+                        </div>
                         <?php }
                         $this->includeTemplate('_partial/footer-part/headerSearchFormArea.php', ['openSerachForm' => true]);
                         ?>
@@ -110,44 +119,34 @@
                             </li>
 
                             <?php if ($controllerName != 'Cart' && (User::isBuyer(true) || (!UserAuthentication::isUserLogged()))) { ?>
-                                <li class="quick-nav-item" id="cartSummaryJs">
-                                    <button class="quick-nav-link button-cart" type="button" data-bs-toggle="offcanvas"
-                                        data-bs-target="#sideCartJs">
-                                        <svg class="svg" width="20" height="20">
-                                            <use
-                                                xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#cart">
-                                            </use>
-                                        </svg>
-                                        <span class="cart-qty">
-                                            <?php
+                            <li class="quick-nav-item" id="cartSummaryJs">
+                                <button class="quick-nav-link button-cart" type="button" data-bs-toggle="offcanvas"
+                                    data-bs-target="#sideCartJs">
+                                    <svg class="svg" width="20" height="20">
+                                        <use
+                                            xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite-header.svg#cart">
+                                        </use>
+                                    </svg>
+                                    <span class="cart-qty">
+                                        <?php
                                             $cartObj = new Cart();
                                             $qty = (Cart::CART_MAX_DISPLAY_QTY < $cartObj->countProducts()) ? Cart::CART_MAX_DISPLAY_QTY . '+' : $cartObj->countProducts();
                                             $qty = FatUtility::int($qty) - (isset($_SESSION['offer_checkout']) ? 1 : 0);
                                             echo (0 > $qty ? 0 : $qty);
                                             ?>
-                                        </span>
-                                        <span class="txt">
-                                            <?php echo Labels::getLabel("LBL_Cart", $siteLangId); ?>
-                                        </span>
-                                    </button>
-                                </li>
+                                    </span>
+                                    <span class="txt">
+                                        <?php echo Labels::getLabel("LBL_Cart", $siteLangId); ?>
+                                    </span>
+                                </button>
+                            </li>
                             <?php }
 
                             if (FatApp::getConfig("CONF_ENABLE_ENGAGESPOT_PUSH_NOTIFICATION", FatUtility::VAR_STRING, '') && UserAuthentication::getLoggedUserId(true) > 0) {
                             ?>
-                                <li class="quick-nav-item">
-                                    <div class="btn-engagespot" id="engagespotUI"></div>
-                                </li>
-                            <?php }
-
-                            if (CommonHelper::demoUrl()) { ?>
-                                <li class="quick-nav-item item-desktop">
-                                    <a class="quick-nav-link btn-cta-outline"
-                                        href="https://www.yo-kart.com/contact-us.html?demo-cta" rel="noopener"
-                                        target="_blank"
-                                        title="Connect with Yo!Kart team to build a Multivendor Marketplace">Start Your
-                                        Marketplace</a>
-                                </li>
+                            <li class="quick-nav-item">
+                                <div class="btn-engagespot" id="engagespotUI"></div>
+                            </li>
                             <?php } ?>
 
                         </ul>
@@ -161,9 +160,9 @@
                     <?php $this->includeTemplate('_partial/headerNavigation.php', ['layoutType' => applicationConstants::SCREEN_DESKTOP]); ?>
                     <?php if ($layoutType == applicationConstants::SCREEN_DESKTOP) {
                         if (0 < FatApp::getConfig('CONF_RFQ_MODULE', FatUtility::VAR_INT, 0) && (User::isBuyer(true) || !UserAuthentication::isUserLogged())) { ?>
-                            <button class="btn btn-brand btn-sm btn-rfq" type="button" onclick="requestForQuoteFn(0);">
-                                <?php echo Labels::getLabel('LBL_REQUEST_FOR_QUOTE', $siteLangId); ?>
-                            </button>
+                    <button class="btn btn-brand btn-rfq" type="button" onclick="requestForQuoteFn(0);">
+                        <?php echo Labels::getLabel('LBL_REQUEST_FOR_QUOTE', $siteLangId); ?>
+                    </button>
                     <?php }
                     } ?>
 
