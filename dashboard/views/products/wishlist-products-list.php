fd38700fb344ea1ca2739036182e97d7186f5d31
Pawan DZ <pawan.kumar.dz@ablysoft.com>
2024-10-29 17:59:34 +0530

    task-120303 worked on dashboard SCSS partitions

--- dashboard/views/products/wishlist-products-list.php
+++ dashboard/views/products/wishlist-products-list.php
@@ -25,29 +25,29 @@ $vtype = $postedData['vtype'] ?? false;
                 $isOutOfMinOrderQty = ((int) ($product['selprod_min_order_qty'] > $availableStock));
                 ?>
 
-        <div class="product-listing-item">
-            <div class="products">
-                <?php $this->includeTemplate('_partial/quick-view.php', ['product' => $product, 'siteLangId' => $siteLangId], false); ?>
-                <?php if ($product['in_stock'] == 0 || 0 < $isOutOfMinOrderQty) { ?>
-                <span class="tag--soldout"><?php echo Labels::getLabel('LBL_SOLD_OUT', $siteLangId); ?></span>
-                <?php } ?>
-                <div class="products-body">
-                    <?php $this->includeTemplate('_partial/collection-ui.php', array('product' => $product, 'siteLangId' => $siteLangId, 'showActionBtns' => ($showActionBtns && false === $isNotServiceable), 'isWishList' => $isWishList, 'selProdRibbons' => $selProdRibbons, 'isOutOfMinOrderQty' => $isOutOfMinOrderQty), false); ?>
-                    <?php if ($isNotServiceable) { ?>
-                    <div class="not-available">
-                        <svg class="svg">
-                            <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#info"
-                                href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#info">
-                            </use>
-                        </svg> <?php echo Labels::getLabel('LBL_NOT_SERVICEABLE', $siteLangId); ?>
-                    </div>
-                    <?php } ?>
-                    <div class="products-img">
-                        <?php $uploadedTime = AttachedFile::setTimeParam($product['product_updated_on']); ?>
-                        <a title="<?php echo $product['selprod_title']; ?>"
-                            href="<?php echo !isset($product['promotion_id']) ? UrlHelper::generateUrl('Products', 'View', array($product['selprod_id']), CONF_WEBROOT_FRONTEND) : UrlHelper::generateUrl('Products', 'track', array($product['promotion_record_id']), CONF_WEBROOT_FRONTEND) ?>">
-                            <?php $fileRow = CommonHelper::getImageAttributes(AttachedFile::FILETYPE_PRODUCT_IMAGE, $product['product_id']); ?>
-                            <?php
+                <div class="product-listing-item">
+                    <div class="products">
+                        <?php $this->includeTemplate('_partial/quick-view.php', ['product' => $product, 'siteLangId' => $siteLangId], false); ?>
+                        <?php if ($product['in_stock'] == 0 || 0 < $isOutOfMinOrderQty) { ?>
+                            <span class="tag--soldout"><?php echo Labels::getLabel('LBL_SOLD_OUT', $siteLangId); ?></span>
+                        <?php } ?>
+                        <div class="products-body">
+                            <?php $this->includeTemplate('_partial/collection-ui.php', array('product' => $product, 'siteLangId' => $siteLangId, 'showActionBtns' => ($showActionBtns && false === $isNotServiceable), 'isWishList' => $isWishList, 'selProdRibbons' => $selProdRibbons, 'isOutOfMinOrderQty' => $isOutOfMinOrderQty), false); ?>
+                            <?php if ($isNotServiceable) { ?>
+                                <div class="not-available">
+                                    <svg class="svg">
+                                        <use xlink:href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#info"
+                                            href="<?php echo CONF_WEBROOT_URL; ?>images/retina/sprite.svg#info">
+                                        </use>
+                                    </svg> <?php echo Labels::getLabel('LBL_NOT_SERVICEABLE', $siteLangId); ?>
+                                </div>
+                            <?php } ?>
+                            <div class="products-img">
+                                <?php $uploadedTime = AttachedFile::setTimeParam($product['product_updated_on']); ?>
+                                <a title="<?php echo $product['selprod_title']; ?>"
+                                    href="<?php echo !isset($product['promotion_id']) ? UrlHelper::generateUrl('Products', 'View', array($product['selprod_id']), CONF_WEBROOT_FRONTEND) : UrlHelper::generateUrl('Products', 'track', array($product['promotion_record_id']), CONF_WEBROOT_FRONTEND) ?>">
+                                    <?php $fileRow = CommonHelper::getImageAttributes(AttachedFile::FILETYPE_PRODUCT_IMAGE, $product['product_id']); ?>
+                                    <?php
                                     $pictureAttr = [
                                         'webpImageUrl' => [ImageDimension::VIEW_DESKTOP => UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'product', array($product['product_id'], 'WEBP' . ImageDimension::VIEW_CLAYOUT3, $product['selprod_id'], 0, $siteLangId), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.webp')],
                                         'jpgImageUrl' => [ImageDimension::VIEW_DESKTOP => UrlHelper::getCachedUrl(UrlHelper::generateFileUrl('image', 'product', array($product['product_id'], ImageDimension::VIEW_CLAYOUT3, $product['selprod_id'], 0, $siteLangId), CONF_WEBROOT_FRONTEND) . $uploadedTime, CONF_IMG_CACHE_TIME, '.jpg')],
@@ -58,25 +58,25 @@ $vtype = $postedData['vtype'] ?? false;
                                     ];
                                     $this->includeTemplate('_partial/picture-tag.php', $pictureAttr, false);
                                     ?>
-                        </a>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="products-foot">
+
+                            <a class="products-category"
+                                href="<?php echo UrlHelper::generateUrl('Category', 'View', array($product['prodcat_id']), CONF_WEBROOT_FRONTEND); ?>"><?php echo html_entity_decode($product['prodcat_name'], ENT_QUOTES, 'UTF-8'); ?>
+                            </a>
+
+                            <a class="products-title" title="<?php echo $product['selprod_title']; ?>"
+                                href="<?php echo UrlHelper::generateUrl('Products', 'View', array($product['selprod_id']), CONF_WEBROOT_FRONTEND); ?>"><?php echo (mb_strlen($product['selprod_title']) > 50) ? mb_substr($product['selprod_title'], 0, 50) . "..." : $product['selprod_title']; ?>
+                            </a>
+
+                            <?php $this->includeTemplate('_partial/collection-product-price.php', array('product' => $product, 'siteLangId' => $siteLangId), false); ?>
+                        </div>
                     </div>
                 </div>
-                <div class="products-foot">
-
-                    <a class="products-category"
-                        href="<?php echo UrlHelper::generateUrl('Category', 'View', array($product['prodcat_id']), CONF_WEBROOT_FRONTEND); ?>"><?php echo html_entity_decode($product['prodcat_name'], ENT_QUOTES, 'UTF-8'); ?>
-                    </a>
-
-                    <a class="products-title" title="<?php echo $product['selprod_title']; ?>"
-                        href="<?php echo UrlHelper::generateUrl('Products', 'View', array($product['selprod_id']), CONF_WEBROOT_FRONTEND); ?>"><?php echo (mb_strlen($product['selprod_title']) > 50) ? mb_substr($product['selprod_title'], 0, 50) . "..." : $product['selprod_title']; ?>
-                    </a>
-
-                    <?php $this->includeTemplate('_partial/collection-product-price.php', array('product' => $product, 'siteLangId' => $siteLangId), false); ?>
-                </div>
-            </div>
-        </div>
-        <?php } ?>
-    </div> <?php
+            <?php } ?>
+        </div> <?php
         $searchFunction = 'goToProductListingSearchPage';
         if (isset($pagingFunc)) {
             $searchFunction = $pagingFunc;
@@ -88,8 +88,8 @@ $vtype = $postedData['vtype'] ?? false;
         $pagingArr = array('pageCount' => $pageCount, 'page' => $postedData['page'], 'recordCount' => $recordCount, 'callBackJsFunc' => $searchFunction);
         $this->includeTemplate('_partial/pagination.php', $pagingArr, false); ?>
     <?php } else { ?>
-</div>
-<?php
+    </div>
+    <?php
     $arr['recordDisplayCount'] = $recordCount;
     echo FatUtility::createHiddenFormFromData($arr, array('name' => 'frmProductSearchPaging', 'id' => 'frmProductSearchPaging'));
     $message = Labels::getLabel('LBL_No_Records_Found', $siteLangId);
