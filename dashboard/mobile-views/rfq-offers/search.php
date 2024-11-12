6a26311182bb2725a8f51820820d76cb189d166c
Satbir <satbir.kaushik@fatbit.in>
2024-11-12 11:47:26 +0530

    task-124268-RFQ offers search with display money format issue

--- dashboard/mobile-views/rfq-offers/search.php
+++ dashboard/mobile-views/rfq-offers/search.php
@@ -1,6 +1,8 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.');
 
 foreach ($arrListing as &$value) {
+    $value['offer_price'] = CommonHelper::displayMoneyFormat($value['offer_price']);
+    $value['counter_offer_price'] = CommonHelper::displayMoneyFormat($value['counter_offer_price']);
     $value['isBought'] = RfqOffers::isBought($value['rlo_accepted_offer_id']) ? 1 : 0;
 }
 
