a7e134daa1c5426d3d56e5900d6349f1e9c79481
Sanchit Sharma <sanchit.sharma@fatbit.in>
2024-12-23 16:09:34 +0530

    bug-089415 - Notification not proper and also not direct the user anywhere

--- application/models/EmailHandler.php
+++ application/models/EmailHandler.php
@@ -3890,7 +3890,7 @@ class EmailHandler extends FatModel
         }
 
         /* Send Notification To Buyer about Bank Transfer transaction status.  */
-        $msg = Labels::getLabel('MSG_ORDER_#{ORDER-ID}_TXN._HAS_BEEN_{STATUS}', $langId);
+        $msg = Labels::getLabel('MSG_ORDER_#{ORDER_ID}_TXN._HAS_BEEN_{STATUS}', $langId);
         $msg = CommonHelper::replaceStringData($msg, $vars);
         $notificationObj = new Notifications();
         $notificationDataArr = array(
