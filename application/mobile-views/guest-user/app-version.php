fb29040a80d43ac360ca5365ac7c68dd84aecc43
Jatinder Singh <jatinder.singh@fatbit.in>
2025-01-29 18:29:22 +0530

    task-129706-Added force update functionality.

---
+++ application/mobile-views/guest-user/app-version.php
@@ -0,0 +1,12 @@
+<?php
+defined('SYSTEM_INIT') or die('Invalid Usage.');
+$response = [
+    'data' => [
+        'app_version' => empty($versionDetails['app_version']) ? '' : $versionDetails['app_version'],
+        'is_version_critical' => empty($versionDetails['is_version_critical']) ? 0 : $versionDetails['is_version_critical'],
+        'version_features' => empty($versionDetails['version_features']) ? '' : $versionDetails['version_features']
+    ]
+];
+
+$msg = Labels::getLabel('LBL_Latest_App_versions', $siteLangId);
+MobileAppUtility::response(MobileAppUtility::STATUS_SUCCESS, $msg, $response);
