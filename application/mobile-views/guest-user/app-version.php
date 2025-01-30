1f6ed54d194df22f6c6cb01c99dfd020d606f53e
Jatinder Singh <jatinder.singh@fatbit.in>
2025-01-30 13:21:42 +0530

    task-129706-Added force update functionality(added app url).

--- application/mobile-views/guest-user/app-version.php
+++ application/mobile-views/guest-user/app-version.php
@@ -4,7 +4,9 @@ $response = [
     'data' => [
         'app_version' => empty($versionDetails['app_version']) ? '' : $versionDetails['app_version'],
         'is_version_critical' => empty($versionDetails['is_version_critical']) ? 0 : $versionDetails['is_version_critical'],
-        'version_features' => empty($versionDetails['version_features']) ? '' : $versionDetails['version_features']
+        'version_features' => empty($versionDetails['version_features']) ? '' : $versionDetails['version_features'],
+        'appstore_url' => empty($versionDetails['appstore_url']) ? '' : $versionDetails['appstore_url']
+        
     ]
 ];
 
