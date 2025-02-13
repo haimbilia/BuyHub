f97a479b6d45578fd0b4ca259823a92adc8cb8f5
Anup Rawat <anup.rawat@ablysoft.com>
2025-02-13 13:19:35 +0530

    resolved conflicts

--- application/models/RequestForQuote.php
+++ application/models/RequestForQuote.php
@@ -810,10 +810,10 @@ class RequestForQuote extends MyAppModel
             return true;
         }
 
-        /* $moduleType = FatApp::getConfig('CONF_RFQ_MODULE_TYPE', FatUtility::VAR_INT, 0);
+        $moduleType = FatApp::getConfig('CONF_RFQ_MODULE_TYPE', FatUtility::VAR_INT, 0);
 
         if ($moduleType != RequestForQuote::TYPE_INDIVIDUAL && SellerProduct::CART_TYPE_RFQ_ONLY == $cartType) {
-            return true; */
+            return true;
         }
 
         return (applicationConstants::YES == $shopRfqEnabled && SellerProduct::CART_TYPE_RFQ_ONLY == $cartType);
