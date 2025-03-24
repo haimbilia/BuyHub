908bb8e68b40ce1dcb529735680cc671950ab2cd
Jatinder Singh <jatinder.singh@fatbit.in>
2025-03-24 14:01:40 +0530

    task-124268 added gdpr policy link in the profile info api.

--- dashboard/mobile-views/account/profile-info.php
+++ dashboard/mobile-views/account/profile-info.php
@@ -4,6 +4,7 @@ $data = array(
     'personalInfo' => (object)$personalInfo,
     'bankInfo' => (object)$bankInfo,
     'privacyPolicyLink' => UrlHelper::generateFullUrl('cms', 'view', array($privacyPolicyLink), CONF_WEBROOT_FRONTEND),
+    'gdprPolicyLink' => UrlHelper::generateFullUrl('cms', 'view', array($gdprPolicyLink), CONF_WEBROOT_FRONTEND),
     'faqLink' => UrlHelper::generateFullUrl('custom', 'faq',[], CONF_WEBROOT_FRONTEND),
     'referralModuleIsEnabled' => FatApp::getConfig("CONF_ENABLE_REFERRER_MODULE", FatUtility::VAR_INT, 0),
     'hasDigitalProducts' => $hasDigitalProducts,
